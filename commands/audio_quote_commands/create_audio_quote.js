d4915515dea67f817bde399271fd2b90a2693480
Weisman <weisman123@outlook.com>
2022-08-09 19:36:04 -0700

    made a basic skeleton to create guild unique slash commands

--- commands/audio_quote_commands/create_audio_quote.js
+++ commands/audio_quote_commands/create_audio_quote.js
@@ -2,8 +2,8 @@ const {errorEmbed} = require('../../functions');
 const {Constants} = require('discord.js');
 const {REST} = require('@discordjs/rest');
 const {Routes} = require('discord.js');
-const {token} = require('./config.json');
 const fs = require('node:fs');
+require('dotenv').config();
 
 module.exports = {
     category:'Audio Quotes',
@@ -23,14 +23,38 @@ module.exports = {
             description: 'Must be a link to an audio file. You can upload the audio file to discord and copy that link.',
             required: true,
             type: Constants.ApplicationCommandOptionTypes.STRING,
+        },
+        {
+            name: 'description',
+            description: 'Add a description to your slash command.',
+            required: true,
+            type: Constants.ApplicationCommandOptionTypes.STRING
         }
     ],
 
     callback: async ({interaction}) => {
         try {
-            const {options} = interaction;
+            const rest = new REST({ version: '10' }).setToken(process.env.TOKEN)
             const guildId = interaction.guildId;
-            
+            const {options} = interaction;
+
+            const name = options.getString('name');
+            const audioFileLink = options.getString('audio file link');
+            const description= options.getString('description');
+
+
+            await rest.put(
+                Routes.applicationGuildCommands(guildId),
+                { body: [{
+                    data: new SlashCommandBuilder()
+                        .setName(name)
+                        .setDescription(description),
+                    async execute(interaction) {
+                        await interaction.reply(audioFileLink);
+                    },
+                }]},
+            );
+
         } catch(err) {
             await interaction.reply(errorEmbed(err));
         };
