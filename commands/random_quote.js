3e956098d7a8b7fa6176fd98b5da96a24a3247d7
Weisman <weisman123@outlook.com>
2022-08-10 03:00:56 -0700

    added filtering to the getRandomQuote command. Mostly just copy and pasting from the filterQuotes command

--- commands/random_quote.js
+++ commands/random_quote.js
@@ -1,33 +1,105 @@
-const {quoteEmbed, errorEmbed, getAuthorById} = require('../functions');
+const {errorEmbed, quoteEmbed, basicEmbed, getAuthorByName, getAuthorById, checkTags} = require('../functions');
 const QuoteSchema = require('../schemas/quote-schema');
+const GuildSchema = require('../schemas/guild-schema');
+const {Constants} = require('discord.js');
+let commandAlreadyRespondedTo = false
 
 module.exports = {
     category:'Quotes',
     description: 'Get a random quote.',
     name: 'randomquote',
     slash: true,
-
+    options: [
+        {
+            name: 'author',
+            description: 'Sort by author of quote.',
+            type: Constants.ApplicationCommandOptionTypes.STRING,
+        },
+        {
+            name: 'first_tag',
+            description: 'Quote must include this tag.',
+            type: Constants.ApplicationCommandOptionTypes.STRING,
+        },
+        {
+            name: 'second_tag',
+            description: 'Quote must include this tag.',
+            type: Constants.ApplicationCommandOptionTypes.STRING
+        },
+        {
+            name: 'third_tag',
+            description: 'Quote must include this tag.',
+            type: Constants.ApplicationCommandOptionTypes.STRING,
+        },
+        {
+            name: 'search_phrase',
+            description: 'A phrase to search for in the quote text.',
+            type: Constants.ApplicationCommandOptionTypes.STRING,
+        },
+    ],
+    
     callback: async ({interaction}) => {
         try {
+            const {options} = interaction;
+            const sortObject = options.getString('date') == null ? {createdAt: -1} : {createdAt: options.getString('date')}
+            const searchPhrase = options.getString('search_phrase')
+            let inputtedAuthor = options.getString('author');
             const guildId = interaction.guildId;
-
-            const amountOfDocuments = await QuoteSchema.countDocuments({ guildId: guildId })
+            const queryObject = {guildId: guildId};
+            
+            if (inputtedAuthor) {
+                inputtedAuthor = await getAuthorByName(inputtedAuthor, guildId);
+            
+                if (inputtedAuthor.name !== 'Deleted Author') {
+                    queryObject.authorId = inputtedAuthor._id;
+                } else {
+                    throw new Error(`'${inputtedAuthor}' author does not exist.`)
+                }
+            }
+            
+            const uncheckedTags = [
+                options.getString('first_tag'),
+                options.getString('second_tag'),
+                options.getString('third_tag'),
+            ];
+            
+            const thereAreTags = uncheckedTags.some(tag => tag !== null);
+            
+            if (thereAreTags) {
+                const guildTags = (await GuildSchema.findOne({guildId: guildId}).select('tags')).tags;
+                let checkedTagsObject = await checkTags(uncheckedTags, guildTags)
+            
+                if (checkedTagsObject.tagsExist) {
+                    queryObject.tags = {$all: checkedTagsObject.checkedTags};
+                } else {
+                    throw new Error('Make sure all your tags exist.')
+                }
+            }
+            
+            if (searchPhrase) {
+                queryObject.$text = {
+                    '$search': searchPhrase
+                }
+            }
+            
+            const amountOfDocuments = await QuoteSchema.countDocuments(queryObject)
 
             if (!amountOfDocuments) {
-                throw new Error('You have no quotes.')
+                throw new Error('Your specifications provide no quotes.')
             }
 
             const randomNumber = Math.floor(Math.random() * amountOfDocuments) + 0;
 
-            const randomQuote = await QuoteSchema.findOne(
-                {guildId: guildId}
-            ).skip(randomNumber).lean()
+            const randomQuote = await QuoteSchema.findOne(queryObject).sort(sortObject).skip(randomNumber).lean()
 
             const author = await getAuthorById(randomQuote.authorId, guildId);
             await interaction.reply(quoteEmbed(randomQuote, author));
 
         } catch(err) {
-            await interaction.reply(errorEmbed(err))
+            if (commandAlreadyRespondedTo) {
+                await interaction.channel.send(errorEmbed(err))
+            } else {
+                await interaction.reply(errorEmbed(err));
+            }
         };
     }
 };
