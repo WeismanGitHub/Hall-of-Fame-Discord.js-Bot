9334828dd12e4b58e6474f4f4b73a1fd095b90a1
Weisman <weisman123@outlook.com>
2022-08-09 20:22:16 -0700

    added a getRandomQuote command

---
+++ commands/random_quote.js
@@ -0,0 +1,34 @@
+const {QuoteEmbed, errorEmbed} = require('../../functions');
+const QuoteSchema = require('../../schemas/quote-schema');
+
+module.exports = {
+    category:'Quotes',
+    description: 'Get a random quote.',
+    name: 'randomquote',
+    slash: true,
+
+    callback: async ({interaction}) => {
+        try {
+            const guildId = interaction.guildId;
+
+            const amountOfDocuments = await QuoteSchema.countDocuments({ guildId: guildId })
+
+            if (!amountOfDocuments) {
+                throw new Error('You have no quotes.')
+            }
+
+            const randomNumber = Math.floor(Math.random() * amountOfDocuments) + 0;
+
+            await QuoteSchema.findOne(
+                {guildId: guildId},
+                {$addToSet: {tags: tag}}
+            ).skip(randomNumber).lean()
+
+            const author = await getAuthorById(randomQuote.authorId, guildId);
+            await interaction.reply(quoteEmbed(randomQuote, author));
+
+        } catch(err) {
+            await interaction.reply(errorEmbed(err))
+        };
+    }
+};
