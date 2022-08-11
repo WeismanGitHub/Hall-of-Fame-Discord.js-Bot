64fb4c55d390a21e8c9a1faa6b3f5b03770c6473
Weisman <weisman123@outlook.com>
2022-08-10 23:44:17 -0700

    updated /help

--- commands/help.js
+++ commands/help.js
@@ -21,7 +21,7 @@ module.exports = {
 
             const descriptionEmbed = new Discord.MessageEmbed()
             .setColor('#5865F2')
-            .setTitle('Description:')
+            .setTitle('Instructions:')
             .setDescription("To create quotes you must first create authors and tags.\nAuthors must have a name and an image url.\nTags are just characters.\nQuotes must have an Author, and can have text and or an attachment url.\nQuotes can have up to 3 tags. You can edit and delete quotes (/editquote, /deletequote).\n\nAudio quotes work exactly the same as regular quotes, but instead of text and or an image, you upload a link to an audio file.\nYou can upload the audio file to discord and copy the link to that.\nThis feature was inspired by the bot AIRHORNSOLUTIONS.\nUsing /playaudioquote will make the bot join the voice chat you're in, play the audio quote, and leave.");
         
             await interaction.reply({embeds: [descriptionEmbed, commandsEmbed]});
