90d3afb42fe3334f138c663c9662374eecb73485
Weisman <weisman123@outlook.com>
2022-08-15 02:53:30 -0700

    made progress in playing audio quotes

--- commands/audio_quote_commands/play_audio_quote.js
+++ commands/audio_quote_commands/play_audio_quote.js
@@ -1,3 +1,4 @@
+const { createAudioPlayer, NoSubscriberBehavior, createAudioResource, joinVoiceChannel } = require('@discordjs/voice');
 const {errorEmbed, quoteEmbed, getAuthorById} = require('../../functions');
 const AudioQuoteSchema = require('../../schemas/audio-quote-schema')
 const {Constants} = require('discord.js');
@@ -41,11 +42,39 @@ module.exports = {
                 ...searchObject
             }).lean()
 
-            const author = await getAuthorById(audioQuote.authorId, guildId)
+            if (!audioQuote) {
+                return await interaction.reply(errorEmbed('Could not find audio quote.'))
+            }
 
+            const voiceChannel = interaction.member.voice.channel
+
+            if (!voiceChannel) {
+                return await interaction.reply(errorEmbed('You must be in a voice channel.'))
+            }
+
+            const player = createAudioPlayer({ behaviors: { noSubscriber: NoSubscriberBehavior.Stop } });
+            
+            player.on('error', error => {
+                console.log(error)
+                throw new Error(error.message)
+            });
+
+            const connection = joinVoiceChannel({
+                channelId: voiceChannel,
+                guildId: interaction.guild.id,
+                adapterCreator: interaction.guild.voiceAdapterCreator
+            });
+
+            const audioQuoteResource = createAudioResource(audioQuote.audioFileLink);
+            connection.subscribe(player);
+            player.play(audioQuoteResource);
+            player.stop();
+
+            const author = await getAuthorById(audioQuote.authorId, guildId)
             await interaction.reply(quoteEmbed(audioQuote, author))
 
         } catch(err) {
+            console.log(err)
             await interaction.reply(errorEmbed(err));
         };
     }
