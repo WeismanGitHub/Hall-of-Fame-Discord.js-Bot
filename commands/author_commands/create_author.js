2b33e7fdcd42df22f06ebe2525306a8b153dc2e7
Weisman <weisman123@outlook.com>
2022-09-03 22:51:26 -0700

    added valid url check

--- commands/author_commands/create_author.js
+++ commands/author_commands/create_author.js
@@ -1,5 +1,6 @@
 const { errorEmbed, authorEmbed } = require('../../helpers/embeds');
 const GuildSchema = require('../../schemas/guild_schema');
+const checkURL = require('../../helpers/check_url')
 const { Constants } = require('discord.js');
 
 module.exports = {
@@ -29,8 +30,12 @@ module.exports = {
             const guildId = interaction.guildId;
             const authorName = options.getString('author');
             const imgUrl = options.getString('icon_url');
-            const authorObject = {name: authorName, imgUrl: imgUrl}
+            const authorObject = { name: authorName, imgUrl: imgUrl }
             
+            if (!checkURL(imgUrl)) {
+                throw new Error('Please input a valid url.')
+            }
+
             await GuildSchema.updateOne(
                 { guildId: guildId },
                 { $addToSet: { authors: { name: authorName, imgUrl: imgUrl } }
