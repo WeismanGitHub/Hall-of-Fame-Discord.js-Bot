f8c4c25a50a52912310243aa687736213ca6fc00
Weisman <weisman123@outlook.com>
2022-09-03 22:13:22 -0700

    fixed checkURL

--- commands/audio_quote_commands/create_audio_quote.js
+++ commands/audio_quote_commands/create_audio_quote.js
@@ -1,5 +1,6 @@
 const AudioQuoteSchema = require('../../schemas/audio_quote_schema')
 const { errorEmbed, quoteEmbed } = require('../../helpers/embeds');
+const checkURL = require('../../helpers/check_url')
 const { getAuthorByName } = require('../../helpers/get_author');
 const { checkTags } = require('../../helpers/check_tags');
 const { Constants } = require('discord.js');
@@ -55,30 +56,34 @@ module.exports = {
             const inputtedAuthor = options.getString('author');
             const checkedAuthor = await getAuthorByName(inputtedAuthor, guildId);
 
-            if (checkedAuthor.name !== 'Deleted Author') {
-                const title = options.getString('title');
-                const audioFileLink = options.getString('audio_file_link');
-
-                let tags = [
-                    options.getString('first_tag'),
-                    options.getString('second_tag'),
-                    options.getString('third_tag'),
-                ];
-    
-                tags = await checkTags(tags, guildId);
-
-                const audioQuote = await AudioQuoteSchema.create({
-                    guildId: guildId,
-                    authorId: checkedAuthor._id,
-                    text: title,
-                    audioFileLink: audioFileLink,
-                    tags: tags,
-                });
-
-                await interaction.reply(quoteEmbed(audioQuote, checkedAuthor))
-            } else {
+            if (checkedAuthor.name == 'Deleted Author') {
                 throw new Error(`Make sure that '${inputtedAuthor}' author exists.`)
             }
+
+            const title = options.getString('title');
+            const audioFileLink = options.getString('audio_file_link');
+
+            if (!checkURL(audioFileLink)) {
+                throw new Error('Please input a valid url.')
+            }
+
+            let tags = [
+                options.getString('first_tag'),
+                options.getString('second_tag'),
+                options.getString('third_tag'),
+            ];
+
+            tags = await checkTags(tags, guildId);
+
+            const audioQuote = await AudioQuoteSchema.create({
+                guildId: guildId,
+                authorId: checkedAuthor._id,
+                text: title,
+                audioFileLink: audioFileLink,
+                tags: tags,
+            });
+
+            await interaction.reply(quoteEmbed(audioQuote, checkedAuthor))
         } catch(err) {
             await interaction.reply(errorEmbed(err));
         };
--- helpers/check_url.js
+++ helpers/check_url.js
@@ -1,9 +1,6 @@
 function checkURL(URL) {
-    try {
-        return Boolean(new URL(URL))
-    } catch(err) {
-        return false
-    }
+    const URL = URL.match(/(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g);
+    return (URL !== null)
 }
 
 module.exports = checkURL
