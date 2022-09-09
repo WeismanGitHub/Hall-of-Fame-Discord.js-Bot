9ba8d3824f6cc423efdefaac7b4851ff166a470c
Weisman <weisman123@outlook.com>
2022-09-09 02:47:10 -0700

    added contact the creator field

--- commands/help.js
+++ commands/help.js
@@ -1,4 +1,5 @@
 const Discord = require('discord.js');
+require('dotenv').config();
 
 module.exports = {
     description: 'help command',
@@ -23,7 +24,7 @@ module.exports = {
             .setColor('#5865F2')
             .setTitle('Instructions:')
             .setDescription("To create quotes you must first create authors and tags.\nAuthors must have a name and an image url.\nTags are just characters.\nQuotes must have an author and text or an attachment url. Or both.\nQuotes can have up to 3 tags. You can edit and delete quotes with /editquote and /deletequote.\n\nAudio quotes work exactly the same as regular quotes, but instead of text/image, you upload a link to an audio/video file.\nDeleting and getting audio quotes is the same, but to edit them use /editaudioquote.\nYou can upload the audio/video file to discord and copy the link to that.\nThis feature was inspired by the bot AIRHORNSOLUTIONS.\nUsing /playaudioquote will make the bot join the voice channel you're in, play the audio quote for max 30 seconds, and leave.")
-            .addFields({ name: 'Contact the Creator:', value: '<@745191242970824735> <@900112979725529108>' })
+            .addFields({ name: 'Contact the Creator:', value: `<@${process.env.MAIN_ACCOUNT_ID}>` })
         
             await interaction.reply({ embeds: [descriptionEmbed, commandsEmbed] });
         } catch(err) {
