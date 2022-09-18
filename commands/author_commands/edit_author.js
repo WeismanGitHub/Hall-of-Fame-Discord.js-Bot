515051ac399f467dcbef0d574ce2756312ff4829
Weisman <weisman123@outlook.com>
2022-09-18 05:16:33 -0700

    removed {new: true} because its no longer necessary

--- commands/author_commands/edit_author.js
+++ commands/author_commands/edit_author.js
@@ -1,4 +1,5 @@
 const { errorEmbed, basicEmbed } = require('../../helpers/embeds');
+const getLastImage = require('../../helpers/get_last_image');
 const GuildSchema = require('../../schemas/guild_schema');
 const checkURL = require('../../helpers/check_url')
 const { Constants } = require('discord.js');
@@ -23,54 +24,64 @@ module.exports = {
             type: Constants.ApplicationCommandOptionTypes.STRING
         },
         {
-            name: 'icon_url',
-            description: 'This will be the image displayed with the author.',
+            name: 'image_link',
+            description: 'This will be the author icon.',
             type: Constants.ApplicationCommandOptionTypes.STRING
+        },
+        {
+            name: 'last_image',
+            description: 'Use the last image sent for the author icon.',
+            type: Constants.ApplicationCommandOptionTypes.CHANNEL
+        },
+        {
+            name: 'delete_image',
+            description: 'Use the default image.',
+            type: Constants.ApplicationCommandOptionTypes.BOOLEAN
         }
     ],
 
     callback: async ({ interaction }) => {
         try {
             const { options } = interaction;
-            const guildId = interaction.guildId;
-            const oldName = options.getString('name');
+            const lastImageChannel = options.getChannel('last_image');
+            const deleteImage = options.getBoolean('delete_image')
+            let newImgUrl = options.getString('icon_url')
             const newName = options.getString('new_name');
-            const newImgUrl = options.getString('icon_url') ?? 'https://cdn.discordapp.com/avatars/973042179033415690/a6602f6209ef6546ee8d878e0022a4f3.webp?size=160'
+            const oldName = options.getString('name');
+            const guildId = interaction.guildId;
 
-            if ((newName == null) && (newImgUrl == null)) {
-                return await interaction.reply(basicEmbed(`Nothing Updated.`));
+            if (!newName && !newImgUrl && !lastImageChannel && !deleteImage) {
+                throw new Error('Please provide an update.')
             }
 
-            if (newImgUrl) {
-                if (!checkURL(newImgUrl)) {
-                    throw new Error('Please input a valid url.')
-                }
-
-                const guildDoc = await GuildSchema.findOneAndUpdate(
-                    { "$and": [
-                        { "guildId": { "$eq": guildId } },
-                        { "authors": { "$elemMatch": { "name": { "$eq": oldName } } } }
-                    ]},
-                    { "$set": { "authors.$.imgUrl": newImgUrl }
-                }).select('_id').lean()
-    
-                if (guildDoc == null) {
-                    throw new Error(`No author named '${oldName}'.`)
-                }
+            if (newImgUrl && !checkURL(newImgUrl)) {
+                throw new Error('Please input a valid url.')
             }
 
-            if (newName) {
-                const guildDoc = await GuildSchema.findOneAndUpdate(
-                    { "$and": [
-                        { "guildId": { "$eq": guildId } },
-                        { "authors": { "$elemMatch": { "name": { "$eq": oldName } } } }
-                    ]},
-                    { "$set": { "authors.$.name": newName }
-                }).select('_id').lean()
-    
-                if (guildDoc == null) {
-                    throw new Error(`No author named '${oldName}'.`)
+            if (lastImageChannel) {
+                newImgUrl = await getLastImage(lastImageChannel)
+            }
+
+            if (deleteImage) {
+                newImgUrl = 'https://cdn.discordapp.com/avatars/973042179033415690/a6602f6209ef6546ee8d878e0022a4f3.webp?size=160'
+            }
+
+            const response = await GuildSchema.updateOne(
+                { "$and": [
+                    { "guildId": { "$eq": guildId } },
+                    { "authors": { "$elemMatch": { "name": { "$eq": oldName } } } }
+                ]},
+                {
+                    "$set": {
+                        "authors.$.imgUrl": newImgUrl,
+                        ...newName && { "authors.$.name": newName }
+                    }
                 }
+            ).select('_id').lean()
+
+            console.log(response)
+            if (response == null) {
+                throw new Error(`No author named '${oldName}'.`)
             }
 
             await interaction.reply(basicEmbed(`Updated '${newName? newName: oldName}'!`));
