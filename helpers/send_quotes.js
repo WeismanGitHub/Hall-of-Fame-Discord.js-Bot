b68f4880160edd8ddeca6cba46034c0f560e271e
Weisman <weisman123@outlook.com>
2022-09-29 02:47:24 -0700

    handles an infinite amount of quotes not only 10

--- helpers/send_quotes.js
+++ helpers/send_quotes.js
@@ -2,23 +2,30 @@ const { quoteEmbed, errorEmbed } = require('../helpers/embeds');
 const { getAuthorById } = require('../helpers/get_author');
 
 async function sendQuotes(quotes, channel) {
-    const quoteEmbeds = []
+    const quoteGroups = [];
 
-    for (let quote of quotes) {
-        const quoteAuthor = await getAuthorById(quote.authorId, channel.guild.id);
-        // Gotta destructure the quote embed because it returns { embeds: [embed] }.
-        quoteEmbeds.push(...quoteEmbed(quote, quoteAuthor).embeds)
+    while (quotes.length > 0) {
+        quoteGroups.push(quotes.splice(0, 10))
     }
 
-    await channel.send({ embeds: quoteEmbeds })
-    .catch(async _ => {
-        for (let quote of quoteEmbeds) {
-            await channel.send({ embeds: [quote] })
-            .catch(async err => {
-                channel.send(errorEmbed(err, `Quote Id: ${quote.fields[0]['value']}`));
-            });
+    for (let quoteGroup of quoteGroups) {
+        const quoteEmbeds = []
+
+        for (let quote of quoteGroup) {
+            const quoteAuthor = await getAuthorById(quote.authorId, channel.guild.id);
+            quoteEmbeds.push(...quoteEmbed(quote, quoteAuthor).embeds)
         }
-    })
+
+        await channel.send({ embeds: quoteEmbeds })
+        .catch(async _ => {
+            for (let quote of quoteEmbeds) {
+                await channel.send({ embeds: [quote] })
+                .catch(async err => {
+                    channel.send(errorEmbed(err, `Quote Id: ${quote.fields[0]['value']}`));
+                });
+            }
+        })
+    };
 }
 
 module.exports = sendQuotes
