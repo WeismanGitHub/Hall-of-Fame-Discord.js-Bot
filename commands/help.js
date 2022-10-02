65f5193d38bc1823615e837870116a8c105f461f
Weisman <weisman123@outlook.com>
2022-10-02 14:51:19 -0700

    updated description and added a button to get command descriptions

--- commands/help.js
+++ commands/help.js
@@ -1,4 +1,5 @@
-const Discord = require('discord.js');
+const { MessageEmbed, MessageActionRow, MessageButton } = require('discord.js');
+const { errorEmbed } = require('../helpers/embeds')
 require('dotenv').config();
 
 module.exports = {
@@ -7,26 +8,22 @@ module.exports = {
     slash: true, 
     category:'Help',
 
-    callback: async ( { instance, interaction } ) => {
+    callback: async ({ interaction }) => {
         try {
-            const blackListedCommands = ['language', 'prefix', 'requiredrole', 'slash', 'command', 'channelonly'];
-            const commandsEmbed = new Discord.MessageEmbed()
+            const row = new MessageActionRow()
+            .addComponents(
+                new MessageButton()
+                .setLabel('Command Descriptions')
+                .setCustomId(',,getCommandDescriptions')
+                .setStyle('PRIMARY')
+            )
+
+            const descriptionEmbed = new MessageEmbed()
             .setColor('#5865F2')
-            .setTitle('Commands:');
-  
-            for await (let command of instance.commandHandler.commands) {
-                if (!blackListedCommands.includes(command.names[0])) {
-                    commandsEmbed.addFields({ name: `${command.names[0]}`, value: `${command.description}` });
-                }
-            }
-            
-            const descriptionEmbed = new Discord.MessageEmbed()
-            .setColor('#5865F2')
-            .setTitle('Instructions:')
-            .setDescription("To create quotes you must first create authors and tags.\nAuthors must have a name and optionally an image.\nQuotes require an author and text or an attachment url. Or both.\nQuotes can have up to 3 tags. You can edit and delete quotes with /editquote and /deletequote.\n\nAudio quotes work exactly the same as regular quotes, but instead of text/image, you upload a link to an audio/video file.\nDeleting and getting audio quotes is the same, but to edit them use /editaudioquote.\nYou can upload the audio/video file to discord and copy the link to that.\nThis feature was inspired by the bot AIRHORNSOLUTIONS.\nUsing /playaudioquote will make the bot join the voice channel you're in, play the audio quote and leave.\nTo get file links you can upload the file to Discord and copy the link to that. You can also use the last_attachment parameter to just use the most recent file sent in the channel.")
+            .setDescription('A hall of fame bot that allows you save text, images, and audio. To create quotes you must first create authors, which have an image and name. Regular quotes have an author and text or an image. Audio quotes have an author, audio/video file, and a title. You can upload a file to Discord and copy the link or use the last_attachment parameter to get the most recent file sent in a channel.')
             .addFields({ name: 'Contact the Creator:', value: `<@${process.env.MAIN_ACCOUNT_ID}>` })
         
-            await interaction.reply({ embeds: [descriptionEmbed, commandsEmbed] });
+            await interaction.reply({ embeds: [descriptionEmbed], components: [row] });
         } catch(err) {
             interaction.reply(errorEmbed(err))
             .catch(_ => interaction.channel.send(errorEmbed(err)))
