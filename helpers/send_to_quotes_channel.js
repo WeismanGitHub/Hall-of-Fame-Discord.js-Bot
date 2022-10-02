89f5412fff1ddff889e1dfeaa155e162b72131be
Weisman <weisman123@outlook.com>
2022-10-02 01:19:05 -0700

    created

---
+++ helpers/send_to_quotes_channel.js
@@ -0,0 +1,17 @@
+const GuildSchema = require('../schemas/guild_schema');
+
+async function updateQuotesChannel(quote, guildId, client) {
+    const quotesChannelId = (await GuildSchema.findOne({ guildId: guildId })
+    .select('quotesChannelId').lean())?.quotesChannelId
+
+    if (quotesChannelId) {
+        const quotesChannel = await client.channels.fetch(quotesChannelId)
+        .catch(err => { throw new Error('Problem sending quotes to quotes channel.') })
+
+        console.log(quotesChannel)
+        await quotesChannel.send(quote)
+    }
+
+}
+
+module.exports = updateQuotesChannel
