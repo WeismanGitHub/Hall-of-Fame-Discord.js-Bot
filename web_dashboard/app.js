34fa1f245f03f43e5612e2729f48a92844242fdc
Weisman <weisman123@outlook.com>
2022-11-23 01:28:34 -0800

    added routes and such

--- web_dashboard/app.js
+++ web_dashboard/app.js
@@ -1,18 +1,31 @@
-const express = require('express');
-require('dotenv').config();
+const rateLimit = require('express-rate-limit')
+const compression = require('compression')
+const mongoose = require('mongoose')
+const apiRouter = require('./api')
+const express = require('express')
+const helmet = require('helmet')
+require('express-async-errors')
+const path = require('path')
+const cors = require('cors')
 
 const app = express();
+const limiter = rateLimit({
+    windowMs: 500,
+	max: 10,
+	standardHeaders: true,
+	legacyHeaders: false,
+})
 
-app.get('/', (req, res) => {
-	return res.sendFile('index.html', { root: '.' });
-});
+app.use(limiter)
+app.use(express.urlencoded({ extended: true }))
+app.use(express.static(path.resolve(__dirname, './build')))
+app.use(express.json())
+app.use(compression())
+app.use(helmet())
+app.use(cors())
 
-app.get('/auth', (req, res) => {
-	return res.sendFile('index.html', { root: '.' });
-});
+app.use('/api/', apiRouter)
 
-app.get('/auth/redirect', (req, res) => {
-	res.status(200).redirect(process.env.REDIRECT_LINK)
-});
+app.get('*', (req, res) => res.sendFile('index.html', { root: path.join(__dirname, './build') }))
 
 module.exports = app
