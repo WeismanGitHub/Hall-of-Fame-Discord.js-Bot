aa16068e7239027c985c74d65f3410337dca3b22
Weisman <weisman123@outlook.com>
2022-11-23 01:29:42 -0800

    updated redirect route

--- web_dashboard/api.js
+++ web_dashboard/api.js
@@ -2,7 +2,7 @@ const { Router } = require('express')
 
 const router = Router()
 
-router.get('/auth/redirect', (req, res) => {
+router.get('/redirect', (req, res) => {
 	res.status(200).redirect(process.env.REDIRECT_LINK)
 });
 
