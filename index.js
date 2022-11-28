602e5ac68b961831fb2ffa69ea9672a8d91f616e
Weisman <weisman123@outlook.com>
2022-11-28 13:18:22 -0800

    fixed port

--- index.js
+++ index.js
@@ -14,7 +14,8 @@ const client = new Client({
     ]
 });
 
-app.listen(process.env.PORT || 5000, () => console.log(`listening on port ${process.env.PORT}...`));
+const port = process.env.PORT || 5000
+app.listen(port || 5000, () => console.log(`listening on port ${port}...`));
 
 client.on("ready", async () => {
     client.user.setActivity('Use /help for help.');
