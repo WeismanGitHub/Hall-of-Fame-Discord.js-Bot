7d88359bdcea87fbc882939f5f8b1b01df9af862
Weisman <weisman123@outlook.com>
2022-11-29 13:59:24 -0800

    fixed routing and error handler location

--- web_dashboard/app.js
+++ web_dashboard/app.js
@@ -21,15 +21,15 @@ app.use(express.urlencoded({ extended: true }))
 app.use(express.static(path.resolve(__dirname, './build')))
 app.use(express.json())
 app.use(compression())
-app.use(errorHandler)
 app.use(helmet())
 app.use(cors())
 
 app.use('/api', apiRouter)
 
-app.get('/', (req, res) => {
+app.get('/*', (req, res) => {
 	res.status(200).sendFile('index.html', { root: path.join(__dirname, './build') })
 });
 
+app.use(errorHandler)
 
 module.exports = app
