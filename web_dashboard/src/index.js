beacc26875600b9f8a5ede9ad192633cb1e1be3c
Weisman <weisman123@outlook.com>
2022-11-29 13:53:55 -0800

    added not found page

--- web_dashboard/src/index.js
+++ web_dashboard/src/index.js
@@ -1,13 +1,18 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
 import React from 'react';
 import './styles.css';
 
 import Home from './home';
+import NotFound from './not-found'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(<>
-    <div className="App">
-        <Home/>
-    </div>
+    <Router>
+        <Routes>
+            <Route exact path='/' element={<Home/>}/>
+            <Route path='*' element={<NotFound/>}/>
+        </Routes>
+    </Router>
 </>);
---
+++ web_dashboard/src/not-found.js
@@ -0,0 +1,9 @@
+function NotFound() {
+    return (<>
+        <br/>
+        <br/>
+        <h1>This page doesn't exist!</h1>
+    </>)
+}
+
+export default NotFound;
