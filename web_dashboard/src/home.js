5efc5e73e99003c985d3c9187f49c7735b1a8400
Weisman <weisman123@outlook.com>
2022-11-30 09:50:45 -0800

    fixed axios

--- web_dashboard/src/home.js
+++ web_dashboard/src/home.js
@@ -1,10 +1,29 @@
 import { useState, useEffect } from 'react';
+import axios, * as others from 'axios'
 
 function Home() {
+    const [loggedIn, setLoggedIn] = useState(false)
+
     useEffect(() => {
-        console.log('HELLO WORLD')
+        const code = String(window.location).split('code=')[1]
+        setLoggedIn(code)
+
+        if (code) {
+            axios.post('/api/login', { code: code })
+        }
     }, [])
-    return <h1>Homepage</h1>
+    
+    if (!loggedIn) {
+        return (<div>
+            <a href={ process.env.REACT_APP_REDIRECT_LINK }>
+                <button>Login to Discord</button>
+            </a>
+        </div>)
+    }
+
+    return (<div>
+        <h1>Home Page</h1>
+    </div>)
 }
 
 export default Home;
