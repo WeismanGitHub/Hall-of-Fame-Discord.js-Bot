3aeccd6bbd52403ef917597684f7f678e1c2593b
Weisman <weisman123@outlook.com>
2022-12-04 04:58:07 -0800

    /guilds saves guilds in jwt

--- web_dashboard/api.js
+++ web_dashboard/api.js
@@ -1,9 +1,10 @@
-const GuildSchema = require('../schemas/guild_schema')
+const GuildSchema = require('../schemas/guild_schema');
 const { PermissionsBitField } = require('discord.js');
 const DiscordOauth2 = require("discord-oauth2");
 const { BadRequestError } = require('./errors')
 const { request } = require('undici');
 const { Router } = require('express')
+const jwt = require('jsonwebtoken')
 require('express-async-errors')
 
 const router = Router()
@@ -37,25 +38,36 @@ router.post('/auth', async (req, res) => {
 		throw new BadRequestError('Problem with code. Try again.')
 	}
 
-	res.status(200).cookie('accessToken', oauthData.access_token).end()
+	console.log(oauthData.access_token)
+	
+	res.status(200).cookie('accessToken', oauthData.access_token, { httpOnly: true, secure: true }).end()
 })
 
 router.get('/guilds', async (req, res) => {
-	try {
-		const guilds = (await oauth.getUserGuilds('req.accessToken')).filter(guild => {
-			const permissions = new PermissionsBitField(guild.permissions)
-			
-			return permissions.has(PermissionsBitField.Flags.USE_APPLICATION_COMMANDS)
-		}).map(guild => {
-			return { id: guild.id, icon: guild.icon }
-		})
-		
-		// icon link = https://cdn.discordapp.com/icons/id/icon.png 
-	
-		res.status(200).send(guilds)
-	} catch(err) {
-		res.status(401).clearCookie('accessToken').send('Invalid Access Token')
+	if (!req.accessToken) {
+		throw new BadRequestError('No Access Token')
 	}
+
+	let guilds = await oauth.getUserGuilds(req.accessToken).catch(err => {
+		return res.status(401).clearCookie('accessToken').send('Invalid Access Token')
+	})
+
+	guilds = guilds.filter(guild => {
+		const userPerms = new PermissionsBitField(guild.permissions)
+		
+		return userPerms.has(PermissionsBitField.Flags.USE_APPLICATION_COMMANDS)
+	}).map(guild => {
+		return { id: guild.id, icon: guild.icon }
+	})
+
+
+	const guildsJWT = jwt.sign(
+        { guilds: guilds },
+        process.env.JWT_SECRET,
+        { expiresIn: process.env.JWT_LIFETIME },
+    )
+
+	res.status(200).cookie('guilds', guildsJWT, { httpOnly: true, secure: true }).json(guilds)
 });
 
 module.exports = router
