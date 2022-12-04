ed040b3d010e6fb26a612a477c649ae7862585c8
Weisman <weisman123@outlook.com>
2022-12-04 06:58:01 -0800

    set up boilerplate for guild display

--- web_dashboard/src/home.js
+++ web_dashboard/src/home.js
@@ -1,31 +1,34 @@
 import { useState, useEffect } from 'react';
 import axios, * as others from 'axios'
 import Cookies from 'js-cookie';
+import Guild from './guild'
 
 function Home() {
     const [loggedIn, setLoggedIn] = useState(false)
-    const [guilds, setGuilds] = useState([])
+    const [guilds, setGuilds] = useState([{ name: 'home', iconURL: '/icon.png' }])
+    const [state, setState] = useState('home')
 
     useEffect(() => {
         const code = String(window.location).split('code=')[1]
         setLoggedIn(code)
 
         if (Cookies.get('loggedIn')) {
-            return axios.get('/api/v1/guilds').then(res => setGuilds(res.data))
+            return axios.get('/api/v1/guilds').then(res => setGuilds(guilds.concat(res.data)))
         }
         
         if (code) {
             axios.post('/api/v1/auth', { code: code })
-            .then(res => axios.get('/api/v1/guilds')).then(res => setGuilds(res.data)).catch(err => setLoggedIn(false))
+            .then(res => axios.get('/api/v1/guilds')).then(res => setGuilds(guilds.concat(res.data))).catch(err => setLoggedIn(false))
         }
 
-        setGuilds(guilds.unshift({ name: 'home', iconURL: '/icon.png' }))
     }, [])
-
+    
     const Home = (<div>
         { guilds.map(guild => {
             return <img class='guild_icon' src={guild.iconURL} alt="img" width="75" height="75" title={`${guild.name}`}></img>
         }) }
+
+        { state !== 'home' ? <Guild guild={ guilds[state] }/> : <div class='guild'><h1>Home</h1></div> }
     </div>)
 
     return (<div>
