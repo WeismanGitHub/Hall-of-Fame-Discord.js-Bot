555b7f12d4bf0f258ddef4e5505c5b6df934bbb0
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2022-12-16 14:19:26 -0800

    added /:guildId route

--- web_dashboard/src/index.js
+++ web_dashboard/src/index.js
@@ -1,23 +1,36 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import ReactDOM from 'react-dom/client';
+import { createRoot } from "react-dom/client";
 import './dist/output.css';
 import React from 'react';
 import './styles.css';
+import {
+    createBrowserRouter,
+    RouterProvider,
+    Route,
+    Link,
+} from "react-router-dom";
 
 import NotFound from './not-found';
 import Guild from './guild';
 import Login from './login';
 import Home from './home';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <Home/>,
+    },
+    {
+        path: '/login',
+        element: <Login/>,
+    },
+    {
+        path: '/:guildId',
+        element: <Guild/>
+    },
+    {
+        path: '*',
+        element: <NotFound/>
+    }
+]);
 
-root.render(<>
-    <Router>
-        <Routes>
-            <Route path='/login' element={<Login/>}/>
-            <Route path='/' element={<Home/>}/>
-            <Route path='/:guildId' element={<Guild/>}/>
-            <Route path='/*' element={<NotFound/>}/>
-        </Routes>
-    </Router>
-</>);
+createRoot(document.getElementById('root')).render(<RouterProvider router={ router }/>)
