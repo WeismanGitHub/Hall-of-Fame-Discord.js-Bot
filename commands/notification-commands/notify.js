2730cc7c9f7a39bd733ac103ad2575f9dd412a2b
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2022-12-18 01:43:05 -0800

    added extra checks

--- commands/notification-commands/notify.js
+++ commands/notification-commands/notify.js
@@ -43,27 +43,37 @@ module.exports = {
         const modalCollector = new InteractionCollector(client, { max: 1 })
 
         modalCollector.on('collect', async interact => {
-            if (interact.isModalSubmit()) {
-                const body = interact.fields.getTextInputValue('body');
-                const title = interact.fields.getTextInputValue('title');
-
-                const guildDocs = await GuildSchema.find({ notifications: true })
-                .select('notificationChannelId').lean()
-    
-                for (let guildDoc of guildDocs) {
-                    const guild = await client.guilds.cache.get(guildDoc._id)
-                    const notificationsChannel = await guild.channels.cache.get(guildDoc.notificationChannelId)
-                    
-                    //user set notification channel, guild system channel, or first channel in server
-                    await (
-                        notificationsChannel ?? guild.systemChannel ?? (guild.channels
-                        .filter(chx => chx.type === 'text')
-                        .find(x => x.position === 0))
-                    ).send(notificationEmbed(title, body))
-                }
-
-                await interaction.reply(basicEmbed('Notification sent!'))
+            if (!interact.isModalSubmit()) {
+                return
             }
+
+            const body = interact.fields.getTextInputValue('body');
+            const title = interact.fields.getTextInputValue('title');
+
+            if (body.length > 4015 || body.length <= 0) { // Max description size is 4096 and 81 characters are already used.
+                throw new Error('Body length must be less than 4015.')
+            }
+
+            if (title.length > 256 || title.length <= 0) { // Max title size is 256.
+                throw new Error('Title length must be less than or equal to 256.')
+            }
+
+            const guildDocs = await GuildSchema.find({ notifications: true })
+            .select('notificationChannelId').lean()
+
+            for (let guildDoc of guildDocs) {
+                const guild = await client.guilds.cache.get(guildDoc._id)
+                const notificationsChannel = await guild.channels.cache.get(guildDoc.notificationChannelId)
+                
+                //user set notification channel, guild system channel, or first channel in server
+                await (
+                    notificationsChannel ?? guild.systemChannel ?? (guild.channels
+                    .filter(chx => chx.type === 'text')
+                    .find(x => x.position === 0))
+                ).send(notificationEmbed(title, body))
+            }
+
+            await interaction.reply(basicEmbed('Notification sent!'))
         })
     })
 };
