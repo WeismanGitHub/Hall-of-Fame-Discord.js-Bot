c56668b1d49073167704eeb47d1f9e224e0f0b2d
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2022-12-20 01:24:57 -0800

    removed mistake

--- commands/help.js
+++ commands/help.js
@@ -27,7 +27,7 @@ module.exports = {
 
         const descriptionEmbed = new MessageEmbed()
         .setColor('#5865F2')
-        .setDescription('A hall of fame bot with 28 commands that allow you save text, images, and audio. The purpose is to allow users to organize memorable moments so they can be easily accessed and found with authors, tags, types, dates, and more. To create quotes you must first create an author for the quote. An author has a name and optionally an image. You can also add tags to quotes to help, which need to be created before hand. Every type of quote (audio, image, regular, and multi) has an author, text, and up to three tags. I strongly recommend you check the Github readme for a more in depth explanation.')
+        .setDescription('A hall of fame bot with 28 commands that allow you save text, images, and audio. The purpose is to allow users to organize memorable moments so they can be easily accessed and found with authors, tags, types, dates, and more. To create quotes you must first create an author for the quote. An author has a name and optionally an image. You can also add tags to quotes, which need to be created beforehand. Every type of quote (audio, image, regular, and multi) has an author, text, and up to three tags. I strongly recommend you check the Github readme for a more in depth explanation.')
         .addFields({ name: 'Contact the Creator:', value: `<@${process.env.MAIN_ACCOUNT_ID}>` })
     
         await interaction.reply({ embeds: [descriptionEmbed], components: [row] });
