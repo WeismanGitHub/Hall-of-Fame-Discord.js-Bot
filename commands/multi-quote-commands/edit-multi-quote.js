cdc54fd43cd68b21022e425652317bc80a2d846d
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2022-12-23 23:19:35 -0800

    finished

--- commands/multi-quote-commands/edit-multi-quote.js
+++ commands/multi-quote-commands/edit-multi-quote.js
@@ -1,7 +1,7 @@
+const { getAuthorByName, getAuthorById } = require('../../helpers/get-author');
 const sendToQuotesChannel = require('../../helpers/send-to-quotes-channel');
-const MultiQuoteSchema = require('../../schemas/audio-quote-schema');
+const MultiQuoteSchema = require('../../schemas/multi-quote-schema');
 const { getLastQuoteId } = require('../../helpers/get-last-item')
-const { getAuthorByName } = require('../../helpers/get-author');
 const errorHandler = require('../../helpers/error-handler');
 const { quoteEmbed } = require('../../helpers/embeds');
 const { Constants } = require('discord.js');
@@ -99,30 +99,60 @@ module.exports = {
             description: 'Removes tags from quote.',
             type: Constants.ApplicationCommandOptionTypes.BOOLEAN
         },
+        {
+            name: 'delete_first_fragment',
+            description: 'Remove a text/author pair.',
+            type: Constants.ApplicationCommandOptionTypes.BOOLEAN
+        },
+        {
+            name: 'delete_second_fragment',
+            description: 'Remove a text/author pair.',
+            type: Constants.ApplicationCommandOptionTypes.BOOLEAN
+        },
+        {
+            name: 'delete_third_fragment',
+            description: 'Remove a text/author pair.',
+            type: Constants.ApplicationCommandOptionTypes.BOOLEAN
+        },
+        {
+            name: 'delete_fourth_fragment',
+            description: 'Remove a text/author pair.',
+            type: Constants.ApplicationCommandOptionTypes.BOOLEAN
+        },
+        {
+            name: 'delete_fifth_fragment',
+            description: 'Remove a text/author pair.',
+            type: Constants.ApplicationCommandOptionTypes.BOOLEAN
+        },
     ],
 
     callback: async ({ interaction, client }) => errorHandler(interaction, async () => {
         const guildId = interaction.guildId;
         const { options } = interaction;
+
+        if (!options._hoistedOptions) {
+            throw new Error('Cannot be empty.')
+        }
+
         const tags = [
             options.getString('first_tag'),
             options.getString('second_tag'),
             options.getString('third_tag'),
         ];
 
-        const id = options.getString('id') ?? await getLastQuoteId(lastQuoteChannel)
         const lastQuoteChannel = options.getChannel('last_quote');
+        const id = options.getString('id') ?? await getLastQuoteId(lastQuoteChannel).catch(err =>  { throw new Error('No Id.') })
         const newTitle = options.getString('new_title')
 
         if (!id) {
             throw new Error('Please provide a quote id or choose a channel to get the quote id from.')
         }
 
-        const multiQuote = await QuoteSchema.findOne({
+        const multiQuote = await MultiQuoteSchema.findOne({
             guildId: guildId,
             _id: id,
             type: 'multi'
-        }).select('_id')
+        })
 
         if (!multiQuote) {
             throw new Error('Multi-quote does not exist.')
@@ -140,34 +170,57 @@ module.exports = {
             multiQuote.text = newTitle;
         }
 
-        // const fragments = [
-        //     { text: options.getString('first_text'), authorName: options.getString('first_author') },
-        //     { text: options.getString('second_text'), authorName: options.getString('second_author') },
-        //     { text: options.getString('third_text'), authorName: options.getString('third_author') },
-        //     { text: options.getString('fourth_text'), authorName: options.getString('fourth_author') },
-        //     { text: options.getString('fifth_text'), authorName: options.getString('fifth_author') },
-        // ]
+        const deleteFragmentOptions = [
+            options.getString('delete_first_fragment'),
+            options.getString('delete_second_fragment'),
+            options.getString('delete_third_fragment'),
+            options.getString('delete_fourth_fragment'),
+            options.getString('delete_fifth_fragment'),
+        ]
 
-        // const checkedFragments = []
+        const newFragments = [
+            { text: options.getString('first_text'), authorName: options.getString('first_author') },
+            { text: options.getString('second_text'), authorName: options.getString('second_author') },
+            { text: options.getString('third_text'), authorName: options.getString('third_author') },
+            { text: options.getString('fourth_text'), authorName: options.getString('fourth_author') },
+            { text: options.getString('fifth_text'), authorName: options.getString('fifth_author') },
+        ]
 
-        // fragments.forEach(async (fragment) => {
-        //     const author = await getAuthorByName(fragment.authorName)
+        const updatedFragments = []
 
-        //     if (author.name == 'Deleted Author') {
-        //         throw new Error(`Make sure that '${fragment.authorName}' author exists.`)
-        //     }
+        for (let i = 0; i < 5; i++) {
+            const oldFragment = multiQuote.toObject().fragments[i]
+            const newFragment = newFragments[i]
+            const updatedFragment = oldFragment
 
-        //     if (!Object.values(fragment).includes(null)) {
-        //         fragment.authorId = author._id
-        //         checkedFragments.push(fragment)
-        //     }
-        // })
+            if (!oldFragment || deleteFragmentOptions[i]) {
+                continue
+            }
 
-        if (!Object.keys(update).length) {
-            throw new Error('No updates.')
+            if (newFragment.authorName) {
+                const author = await getAuthorByName(newFragment.authorName, guildId)
+                updatedFragment.authorName = newFragment.authorName
+
+                if (author.name == 'Deleted Author') {
+                    throw new Error(`Make sure that '${newFragment.authorName}' author exists.`)
+                }
+
+                updatedFragment.authorId = author._id
+            } else {
+                updatedFragment.authorName = (await getAuthorById(oldFragment.authorId, guildId)).name
+            }
+            
+            if (newFragment.text) {
+                updatedFragment.text = newFragment.text
+            }
+
+            updatedFragments.push(updatedFragment)
         }
 
-        const embeddedMultiQuote = quoteEmbed(multiQuote, checkedFragments) // checkedFragments
+        multiQuote.fragments = updatedFragments
+        await multiQuote.save()
+
+        const embeddedMultiQuote = quoteEmbed(multiQuote, updatedFragments)
 
         await sendToQuotesChannel(embeddedMultiQuote, guildId, client)
         await interaction.reply(embeddedMultiQuote);
