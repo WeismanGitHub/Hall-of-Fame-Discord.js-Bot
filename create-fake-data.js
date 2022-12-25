0a599f55ec4b9d146be0566dd43ff9358479d2f9
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2022-12-25 01:33:53 -0800

    updated

--- create-fake-data.js
+++ create-fake-data.js
@@ -1,11 +1,25 @@
+const MultiQuoteSchema = require('./schemas/multi-quote-schema')
 const AudioQuoteSchema = require('./schemas/audio-quote-schema')
 const QuoteSchema = require('./schemas/quote-schema')
 const GuildSchema = require('./schemas/guild-schema')
 const { faker } = require('@faker-js/faker');
+const mongoose = require('mongoose');
+require('dotenv').config();
 
-async function createFakeData(guildAmount = 5, authorsPerGuild = 10, tagsPerGuild = 10, regQuotesPerAuthor = 10, audioQuotesPerAuthor = 2) {
-    const authorIconURL = 'https://cdn.discordapp.com/avatars/973042179033415690/a6602f6209ef6546ee8d878e0022a4f3.png?size=4096'
-    const audioQuoteLink = 'https://media.discordapp.net/attachments/1025999685787336725/1031490055946129408/futurama-zoidberg-1.mp4'
+mongoose.connect(process.env.FAKE_DATA_MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+
+async function createFakeData(
+    guildAmount = 5,
+    authorsPerGuild = 10,
+    tagsPerGuild = 10,
+    regQuotesPerAuthor = 10,
+    audioQuotesPerAuthor = 5,
+    multiQuotesPerAuthor = 2
+) {
+    const fakeURL = 'https://fake.url'
 
     for (let i = 0; i < guildAmount; i++) {
         const authors = []
@@ -16,53 +30,72 @@ async function createFakeData(guildAmount = 5, authorsPerGuild = 10, tagsPerGuil
         }
 
         for (let k = 0; k < authorsPerGuild; k++) {
-            authors.push({ name: `${faker.name.firstName()} ${faker.name.lastName()}`, iconURL: authorIconURL })
+            authors.push({ name: faker.name.firstName(), iconURL: fakeURL })
         }
 
         const guild = await GuildSchema.create({
-            _id: faker.mersenne.rand(100000000000000, 999999999999999),
+            _id: faker.datatype.number({ min: 100000000000000, max: 999999999999999 }),
             tags: tags,
             authors: authors
         })
 
         for (const author of guild.authors) {
+            const regularQuotes = []
             const audioQuotes = []
-            const quotes = []
+            const multiQuotes = []
             
-            for (let l = 0; l < regQuotesPerAuthor; l++) {
-                const attachment = Math.floor(Math.random() * 10) % 2 ? { attachment: authorIconURL } : null
-                const tagsNumber = Math.floor(Math.random() * tagsPerGuild + 1)
+            for (let l = 0; l < regQuotesPerAuthor; l++) { // Regular Quotes
+                const tagAmount = faker.datatype.number({ 'min': 0, 'max': tagsPerGuild });
+                const imageURL = faker.datatype.number({ 'min': 0, 'max': 1 }) ? fakeURL : null
                 let text = null;
                 
-                if (attachment == null || Math.floor(Math.random()*10) % 2) {
-                    text = { text: faker.lorem.sentence() }
+                if (imageURL == null || faker.datatype.number({ 'min': 0, 'max': 1 })) {
+                    text = faker.lorem.sentence()
                 }
     
-                quotes.push({
-                    _id: guild._id,
+                regularQuotes.push({
+                    guildId: guild._id,
                     authorId: author._id,
-                    ...attachment,
-                    ...text,
-                    tags: tags.slice(tagsNumber, tagsNumber + Math.floor(Math.random() * 4))
+                    attachmentURL: imageURL,
+                    text: text,
+                    tags: tags.slice(tagAmount, tagAmount + faker.datatype.number({ 'min': 0, 'max': 3 }))
                 })
             }
             
-            for (let m = 0; m < audioQuotesPerAuthor; m++) {
-                const tagsNumber = Math.floor(Math.random() * tagsPerGuild + 1)
+            for (let m = 0; m < audioQuotesPerAuthor; m++) { // Audio Quotes
+                const tagAmount = faker.datatype.number({ 'min': 0, 'max': tagsPerGuild });
                 
                 audioQuotes.push({
-                    _id: guild._id,
+                    guildId: guild._id,
                     authorId: author._id,
-                    audioFileLink: audioQuoteLink,
+                    audioURL: fakeURL,
                     text: faker.lorem.sentence(),
-                    tags: tags.slice(tagsNumber, tagsNumber + Math.floor(Math.random() * 4))
+                    tags: tags.slice(tagAmount, tagAmount + faker.datatype.number({ 'min': 0, 'max': 3 }))
+                })
+            }
+
+            for (let n = 0; n < multiQuotesPerAuthor; n++) { // Multi Quotes
+                const tagAmount = faker.datatype.number({ 'min': 0, 'max': tagsPerGuild });
+                const fragmentAmount = faker.datatype.number({ 'min': 2, 'max': 5 });
+                const fragments = []
+
+                for (let o = 0; o < fragmentAmount; o++) {
+                    fragments.push({ authorId: author._id, text: faker.lorem.sentence() })
+                }
+
+                multiQuotes.push({
+                    guildId: guild._id,
+                    fragments: fragments,
+                    text: faker.lorem.sentence(),
+                    tags: tags.slice(tagAmount, faker.datatype.number({ 'min': 0, 'max': 3 }))
                 })
             }
 
             await AudioQuoteSchema.create(audioQuotes)
-            await QuoteSchema.create(quotes)
+            await MultiQuoteSchema.create(multiQuotes)
+            await QuoteSchema.create(regularQuotes)
         }
     }
 }
 
-module.exports = createFakeData
+createFakeData().then(console.log('Done!'))
