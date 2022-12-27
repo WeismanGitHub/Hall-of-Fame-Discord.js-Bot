f238fea2640d94ad125af993bde71b7c181a92a1
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2022-12-26 21:34:26 -0800

    created author description

--- descriptions.js
+++ descriptions.js
@@ -1,5 +1,7 @@
-const tagDescription = 'Tags are used for filtering.'
+const tagDescription = 'Tags are predefined and used for finding quotes.'
+const authorDescription = 'Authors are predefined and used for finding quotes.'
 
 module.exports = {
-    tagDescription
+    tagDescription,
+    authorDescription,
 }
