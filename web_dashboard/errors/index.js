dcb78b75c2a6dcc666fc3fe5fa8f31f75fcb0ec4
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-01-02 12:41:08 -0800

    added unathorized error

--- web_dashboard/errors/index.js
+++ web_dashboard/errors/index.js
@@ -1,8 +1,10 @@
+const UnauthorizedError = require('./unauthorized');
 const BadRequestError = require('./bad-request');
 const CustomError = require('./custom-error');
 const NotFoundError = require('./not-found');
 
 module.exports = {
+    UnauthorizedError,
     BadRequestError,
     NotFoundError,
     CustomError,
