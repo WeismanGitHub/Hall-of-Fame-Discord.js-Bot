833383db0e4798428b174fa40fe0ac9c54e92020
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-01-14 21:57:18 -0800

    got sorta working quotes

--- web-dashboard/src/quotes.js
+++ web-dashboard/src/quotes.js
@@ -1,8 +1,39 @@
+import * as moment from 'moment-timezone';
+
 function Quotes({ quotes, authors }) {
+    if (!quotes.length || !quotes) {
+        return <div class='no_quotes'>
+            404: No Quotes
+        </div>
+    }
+
     return <div class='quotes'>
-        { quotes.map(quote => <div class='quote'>
-            <h1>{ quote.text }</h1>
-        </div>) }
+        { quotes.map(quote => {
+            const author = authors.find(author => author._id == quote.authorId)
+            const { type, attachmentURL, createdAt, text } = quote
+
+            if (type == 'regular' && !attachmentURL) {
+                return <>
+                    <div class='quote'>
+                        <img
+                            class='quote_author_icon'
+                            src={ author?.iconURL || "/icon.png" }
+                            alt="author icon"
+                            width = "50"
+                            height = "50"
+                            onError={({ currentTarget }) => {
+                                currentTarget.onerror = null; // prevents looping
+                                currentTarget.src="/icon.png";
+                            }}
+                        />
+                        <div class='quote_author_name'>{ author?.name || 'Deleted Author' }</div>
+                        <div class='quote_time'>{ moment(createdAt).calendar() }</div>
+                        <div class='quote_text'>{ text }</div>
+                    </div>
+                    <br/>
+                </>
+            }
+        }) }
     </div>
 }
 
