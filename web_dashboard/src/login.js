c272014cefbce24dfe86a44e253ec70c339e8987
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-01-16 01:04:17 -0800

    deleted

--- web_dashboard/src/login.js
+++
@@ -1,7 +0,0 @@
-function Login() {
-    return <a href={ process.env.REACT_APP_LOCALHOST_LINK }>
-        <button>Discord Login</button>
-    </a>
-}
-
-export default Login
--- web_dashboard/src/not-found.js
+++
@@ -1,7 +0,0 @@
-function NotFound() {
-    return (<>
-        <h1>This page doesn't exist!</h1>
-    </>)
-}
-
-export default NotFound;
