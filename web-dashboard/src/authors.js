6bcb35c12acd4727b58f2c03e98661816e560e5c
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-02-04 03:22:44 -0800

    delete author with right click

--- web-dashboard/src/authors.js
+++ web-dashboard/src/authors.js
@@ -1,6 +1,39 @@
-function Authors({ authors, setQueryAuthorId, queryAuthorId }) {
-    function authorClick(id) {
+import axios, * as others from 'axios'
+import { toast } from 'react-toastify'
+
+function Authors({ authors, setQueryAuthorId, queryAuthorId, guildId }) {
+    function selectAuthor(id) {
         queryAuthorId == id ? setQueryAuthorId(null) : setQueryAuthorId(id)
+    }
+    
+    function deleteAuthor(name, id) {
+        if (window.confirm(`Delete "${name}"?`)) {
+            axios.delete(`/api/v1/authors/${guildId}`, { id: id })
+            .then(res => {
+                toast.success(`Successfully deleted "${name}".`, {
+                    position: "top-right",
+                    autoClose: 3000,
+                    hideProgressBar: true,
+                    closeOnClick: true,
+                    pauseOnHover: true,
+                    draggable: true,
+                    progress: undefined,
+                    theme: "colored",
+                })
+            })
+            .catch(err => {
+                toast.error(err.message || `Failed to delete "${name}".`, {
+                    position: "top-right",
+                    autoClose: 3000,
+                    hideProgressBar: true,
+                    closeOnClick: true,
+                    pauseOnHover: true,
+                    draggable: true,
+                    progress: undefined,
+                    theme: "colored",
+                })
+            })
+        }
     }
 
     return <div class='authors'>
@@ -9,10 +42,14 @@ function Authors({ authors, setQueryAuthorId, queryAuthorId }) {
         <br/>
         { authors.map(author => <div class='author'>
             <div class={ queryAuthorId == author._id ? 'highlighted' : 'unhighlighted'}>
-                <div class='author_container' onClick={ () => authorClick(author._id) }>
+                <div
+                    class='author_container'
+                    onClick={() => selectAuthor(author._id)}
+                    onContextMenu={(e) => {e.preventDefault(); deleteAuthor(author.name, author._id)}}
+                >
                     <img
                         class='author_icon'
-                        src={ author?.iconURL || "/icon.png" }
+                        src={author?.iconURL || "/icon.png"}
                         alt="author icon"
                         width = "45"
                         height = "45"
@@ -21,7 +58,7 @@ function Authors({ authors, setQueryAuthorId, queryAuthorId }) {
                             currentTarget.src="/icon.png";
                         }}
                     />
-                    <div class='author_name'>{ author.name }</div>
+                    <div class='author_name'>{author.name}</div>
                 </div>
             </div>
             <br/>
