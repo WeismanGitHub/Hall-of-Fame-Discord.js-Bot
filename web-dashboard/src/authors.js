95af3cfeb09ae860da96a783f649d952aa1a2d23
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-02-05 12:00:23 -0800

    added context menu to authors

--- web-dashboard/src/authors.js
+++ web-dashboard/src/authors.js
@@ -1,14 +1,39 @@
+import { Menu, Item, useContextMenu } from 'react-contexify';
+import 'react-contexify/ReactContexify.css';
 import axios, * as others from 'axios'
 import { toast } from 'react-toastify'
 
 function Authors({ authors, setQueryAuthorId, queryAuthorId, guildId }) {
+    const authorContextId = 'author_id'
+
     function selectAuthor(id) {
         queryAuthorId == id ? setQueryAuthorId(null) : setQueryAuthorId(id)
     }
     
-    function deleteAuthor(name, id) {
-        if (window.confirm(`Delete "${name}"?`)) {
-            axios.delete(`/api/v1/authors/${guildId}`, { id: id })
+    const { show } = useContextMenu({
+        id: authorContextId
+    });
+
+    function handleContextMenu(event, props) {
+        show({ event, props })
+    }
+
+    const handleItemClick = ({ id, props }) => {
+        const { name, authorId } = props
+
+        switch (id) {
+        case "copy":
+            navigator.clipboard.writeText(name)
+            break;
+        case "edit":
+            alert('Editing coming soon.')
+            break;
+        case "delete":
+            if (!window.confirm(`Delete "${name}"?`)) {
+                break
+            }
+
+            axios.delete(`/api/v1/authors/${guildId}`, { id: authorId })
             .then(res => {
                 toast.success(`Successfully deleted "${name}".`, {
                     position: "top-right",
@@ -20,8 +45,7 @@ function Authors({ authors, setQueryAuthorId, queryAuthorId, guildId }) {
                     progress: undefined,
                     theme: "colored",
                 })
-            })
-            .catch(err => {
+            }).catch(err => {
                 toast.error(err.message || `Failed to delete "${name}".`, {
                     position: "top-right",
                     autoClose: 3000,
@@ -33,6 +57,7 @@ function Authors({ authors, setQueryAuthorId, queryAuthorId, guildId }) {
                     theme: "colored",
                 })
             })
+            break;
         }
     }
 
@@ -40,16 +65,15 @@ function Authors({ authors, setQueryAuthorId, queryAuthorId, guildId }) {
         <div class='authors_header'>Authors - { authors.length }</div>
         <hr class="authors_divider"/>
         <br/>
-        { authors.map(author => <div class='author'>
-            <div class={ queryAuthorId == author._id ? 'highlighted' : 'unhighlighted'}>
-                <div
-                    class='author_container'
-                    onClick={() => selectAuthor(author._id)}
-                    onContextMenu={(e) => {e.preventDefault(); deleteAuthor(author.name, author._id)}}
+        { authors.map(({ _id, iconURL, name }) => <div class='author'>
+            <div class={ queryAuthorId == _id ? 'highlighted' : 'unhighlighted'}>
+                <div class='author_container'
+                    onClick={() => selectAuthor(_id)}
+                    onContextMenu={(e) => handleContextMenu(e, { name, authorId: _id })}
                 >
                     <img
                         class='author_icon'
-                        src={author?.iconURL || "/icon.png"}
+                        src={iconURL || "/icon.png"}
                         alt="author icon"
                         width = "45"
                         height = "45"
@@ -58,8 +82,13 @@ function Authors({ authors, setQueryAuthorId, queryAuthorId, guildId }) {
                             currentTarget.src="/icon.png";
                         }}
                     />
-                    <div class='author_name'>{author.name}</div>
+                    <div class='author_name'>{name}</div>
                 </div>
+                <Menu id={authorContextId} theme="dark">
+                    <Item id="copy" props={name} onClick={handleItemClick}>Copy</Item>
+                    <Item id="edit" onClick={handleItemClick}>Edit</Item>
+                    <Item id="delete" props={{name, _id}} onClick={handleItemClick}>Delete</Item>
+                </Menu>
             </div>
             <br/>
         </div>) }
