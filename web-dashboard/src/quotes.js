7e93a802203d9422136c70c17398fde15ed742f2
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-02-07 19:05:07 -0800

    added context menu to quotes

--- web-dashboard/src/quotes.js
+++ web-dashboard/src/quotes.js
@@ -1,8 +1,18 @@
+import { Menu, Item, useContextMenu } from 'react-contexify';
 import InfiniteScroll from "react-infinite-scroller";
+import { successToast, errorToast } from './toasts';
 import AudioPlayer from 'react-h5-audio-player';
+import 'react-contexify/ReactContexify.css';
 import * as moment from 'moment-timezone';
+import axios, * as others from 'axios'
 
-function Quotes({ loadMoreQuotes, quotes, authors, queryPage, setQueryPage }) {
+function Quotes({ loadMoreQuotes, quotes, authors, queryPage, setQueryPage, guildId, setQuotes }) {
+    const quoteContextId = 'quote_id'
+
+    const { show } = useContextMenu({
+        id: quoteContextId
+    });
+
     if (!quotes?.length || !quotes) {
         return <div class='no_quotes'>
             404: No Quotes
@@ -13,7 +23,32 @@ function Quotes({ loadMoreQuotes, quotes, authors, queryPage, setQueryPage }) {
         setQueryPage(queryPage + 1)
         loadMoreQuotes()
     }
-    
+
+    function handleContextMenu(event, props) {
+        show({ event, props })
+    }
+
+    const handleItemClick = ({ id, props }) => {
+        switch (id) {
+        case "edit":
+            alert('Editing coming soon.')
+            break;
+        case "delete":
+            if (!window.confirm(`Delete quote?`)) {
+                break
+            }
+
+            axios.delete(`/api/v1/${guildId}/quotes/${props.id}`)
+            .then(res => {
+                successToast(`Successfully deleted quote.`)
+                setQuotes(quotes.filter(quote => quote._id !== props.id))
+            }).catch(err => {
+                errorToast(`Failed to delete quote.`)
+            })
+            break;
+        }
+    }
+
     return <div class='quotes'>
         <InfiniteScroll
             pageStart={ queryPage }
@@ -25,7 +60,7 @@ function Quotes({ loadMoreQuotes, quotes, authors, queryPage, setQueryPage }) {
         >
             { quotes.map(quote => {
                 const author = authors.find(author => author._id == quote.authorId)
-                const { type, attachmentURL, createdAt, text, audioURL, fragments } = quote
+                const { type, attachmentURL, createdAt, text, audioURL, fragments, _id } = quote
                 const filteredTags = quote.tags.filter(x => x !== null)
                 const tags = filteredTags.length ? filteredTags.join(', ') : ['no tags']
                 let color;
@@ -45,42 +80,103 @@ function Quotes({ loadMoreQuotes, quotes, authors, queryPage, setQueryPage }) {
                 color = attachmentURL ? '#FF7B00' : color
                 
                 if (type !== 'multi') {
-                        return <div className='quote_message' style={{ 'border-left': `4px solid ${color}` }}>
-                            <div className="quote_author_avatar">
-                                <img
-                                    src={ author?.iconURL || "/icon.png" }
-                                    alt="author icon"
-                                    width = "50"
-                                    height = "50"
-                                    onError={({ currentTarget }) => {
-                                        currentTarget.onerror = null; // prevents looping
-                                        currentTarget.src="/icon.png";
-                                    }}
-                                />
+                        return <>
+                            <div className='quote_message'
+                            style={{ 'border-left': `4px solid ${color}` }}
+                            onContextMenu={(e) => handleContextMenu(e, { id: _id })}>
+                                <div className="quote_author_avatar">
+                                    <img
+                                        src={ author?.iconURL || "/icon.png" }
+                                        alt="author icon"
+                                        width = "50"
+                                        height = "50"
+                                        onError={({ currentTarget }) => {
+                                            currentTarget.onerror = null; // prevents looping
+                                            currentTarget.src="/icon.png";
+                                        }}
+                                    />
+                                </div>
+                                <div className="quote_message_content">
+                                    <div>
+                                        <span className="quote_author_info">
+                                            <span className="quote_author_username">{ author?.name || 'Deleted Author' }</span>
+                                        </span>
+                                        <span className="quote_message_timestamp">
+                                            { moment(createdAt).calendar() } - { tags }
+                                        </span>
+                                    </div>
+
+                                    <div className="quote_message_body">
+                                        { text }
+                                        
+                                        { !audioURL ? null : <AudioPlayer
+                                            src={audioURL}
+                                            showJumpControls={false}
+                                            showSkipControls={false}
+                                            autoPlayAfterSrcChange={false}
+                                            hasDefaultKeyBindings={false}
+                                            showFilledProgress={true}
+                                            layout={'horizontal'}
+                                            customVolumeControls={[]}
+                                        /> }
+
+                                        { !attachmentURL ? null : <img
+                                            class='quote_image'
+                                            src={ attachmentURL }
+                                            alt="invalid image"
+                                        />}
+                                    </div>
+                                </div>
                             </div>
+                            <Menu id={quoteContextId} theme="dark">
+                                <Item id="edit" onClick={handleItemClick}>Edit</Item>
+                                <Item id="delete" onClick={handleItemClick}>Delete</Item>
+                            </Menu>
+                        </>
+                } else {
+                    return <>
+                        <div className='quote_message'
+                        style={{ 'border-left': `4px solid ${color}` }}
+                        onContextMenu={(e) => handleContextMenu(e, { id: _id })}>
                             <div className="quote_message_content">
                                 <div>
-                                    <span className="quote_author_info">
-                                        <span className="quote_author_username">{ author?.name || 'Deleted Author' }</span>
+                                    <span className="quote_author_info" style={{ 'margin-left': '10px' }}>
+                                        <span className="quote_author_username">{ text }</span>
                                     </span>
                                     <span className="quote_message_timestamp">
                                         { moment(createdAt).calendar() } - { tags }
                                     </span>
                                 </div>
-
                                 <div className="quote_message_body">
-                                    { text }
-                                    
-                                    { !audioURL ? null : <AudioPlayer
-                                        src={audioURL}
-                                        showJumpControls={false}
-                                        showSkipControls={false}
-                                        autoPlayAfterSrcChange={false}
-                                        hasDefaultKeyBindings={false}
-                                        showFilledProgress={true}
-                                        layout={'horizontal'}
-                                        customVolumeControls={[]}
-                                    /> }
+                                    { fragments.map(({ authorId, text })=> {
+                                        const author = authors.find(author => author._id == authorId)
+
+                                        return <div className='quote_message'>
+                                            <div className="quote_author_avatar">
+                                                <img
+                                                    src={ author?.iconURL || "/icon.png" }
+                                                    alt="author icon"
+                                                    width = "40"
+                                                    height = "40"
+                                                    onError={({ currentTarget }) => {
+                                                        currentTarget.onerror = null; // prevents looping
+                                                        currentTarget.src="/icon.png";
+                                                    }}
+                                                />
+                                            </div>
+                                            <div className="quote_message_content">
+                                                <div>
+                                                    <span className="quote_author_info">
+                                                        <span className="quote_author_username">{ author?.name || 'Deleted Author' }</span>
+                                                    </span>
+                                                </div>
+
+                                                <div className="quote_message_body">
+                                                    { text }
+                                                </div>
+                                            </div>
+                                        </div>
+                                    })}
 
                                     { !attachmentURL ? null : <img
                                         class='quote_image'
@@ -90,56 +186,11 @@ function Quotes({ loadMoreQuotes, quotes, authors, queryPage, setQueryPage }) {
                                 </div>
                             </div>
                         </div>
-                } else {
-                    return <div className='quote_message' style={{ 'border-left': `4px solid ${color}` }}>
-                        <div className="quote_message_content">
-                            <div>
-                                <span className="quote_author_info" style={{ 'margin-left': '10px' }}>
-                                    <span className="quote_author_username">{ text }</span>
-                                </span>
-                                <span className="quote_message_timestamp">
-                                    { moment(createdAt).calendar() } - { tags }
-                                </span>
-                            </div>
-                            <div className="quote_message_body">
-                                { fragments.map(({ authorId, text })=> {
-                                    const author = authors.find(author => author._id == authorId)
-
-                                    return <div className='quote_message'>
-                                        <div className="quote_author_avatar">
-                                            <img
-                                                src={ author?.iconURL || "/icon.png" }
-                                                alt="author icon"
-                                                width = "40"
-                                                height = "40"
-                                                onError={({ currentTarget }) => {
-                                                    currentTarget.onerror = null; // prevents looping
-                                                    currentTarget.src="/icon.png";
-                                                }}
-                                            />
-                                        </div>
-                                        <div className="quote_message_content">
-                                            <div>
-                                                <span className="quote_author_info">
-                                                    <span className="quote_author_username">{ author?.name || 'Deleted Author' }</span>
-                                                </span>
-                                            </div>
-
-                                            <div className="quote_message_body">
-                                                { text }
-                                            </div>
-                                        </div>
-                                    </div>
-                                })}
-
-                                { !attachmentURL ? null : <img
-                                    class='quote_image'
-                                    src={ attachmentURL }
-                                    alt="invalid image"
-                                />}
-                            </div>
-                        </div>
-                    </div>
+                        <Menu id={quoteContextId} theme="dark">
+                            <Item id="edit" onClick={handleItemClick}>Edit</Item>
+                            <Item id="delete" onClick={handleItemClick}>Delete</Item>
+                        </Menu>
+                    </>
                 }
             })}
         </InfiniteScroll>
