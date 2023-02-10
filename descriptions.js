542edfcb9514b2b72553b02eae3ecfc4f0e79217
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-02-09 20:44:32 -0800

    added more descriptions

--- descriptions.js
+++ descriptions.js
@@ -1,7 +1,13 @@
-const tagDescription = 'Tags are predefined and used for finding quotes.'
-const authorDescription = 'Authors are predefined and used for finding quotes.'
+const tagDescription = 'Tags must be predefined and are used for finding quotes.'
+const authorDescription = 'Authors must be predefined and are used for finding quotes.'
+const newNameDescription = 'What you want the new name to be.'
+const searchPhraseDescription = 'The text you want to search for.'
+const typeDescription = 'The type of quote you want.'
 
 module.exports = {
     tagDescription,
     authorDescription,
+    newNameDescription,
+    searchPhraseDescription,
+    typeDescription
 }
