eae1bdd12f16b041dd35d1b0dc2ec3eabac9254d
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-02-14 00:16:33 -0800

    added authorization middleware to all /api/v1/:guildId/* routes

--- web-dashboard/app.js
+++ web-dashboard/app.js
@@ -1,9 +1,11 @@
+const { ForbiddenError } = require('../errors')
 const rateLimit = require('express-rate-limit')
 const errorHandler = require('./error-handler')
 const { NotFoundError } = require('./errors')
 const cookieParser = require('cookie-parser')
 const compression = require('compression')
 const apiRouter = require('./api-router')
+const jwt = require('jsonwebtoken')
 const express = require('express')
 const helmet = require('helmet')
 require('express-async-errors')
@@ -34,7 +36,17 @@ app.use(cors({ origin: ['http://localhost:5000'] }))
 app.use(express.urlencoded({ extended: true }))
 app.use(cookieParser())
 app.use(express.json())
-	
+
+app.use('/api/v1/:guildId/*', (req, res, next) => {
+	req.guilds = jwt.verify(req.cookies.guilds, process.env.JWT_SECRET)
+
+	if (!req.guilds.includes(req.params.guildId)) {
+		throw new ForbiddenError('Invalid Guild Id')
+	}
+
+	next()
+})
+
 app.use('/api/v1', apiRouter)
 app.use(express.static(path.resolve(__dirname, './build')))
 
