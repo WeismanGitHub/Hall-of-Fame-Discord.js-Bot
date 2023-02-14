1382f8b2ae0dab41031ece6c66ad43134fa0b93b
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-02-13 23:10:20 -0800

    added edit tag popup

--- web-dashboard/src/view/authors.js
+++ web-dashboard/src/view/authors.js
@@ -2,8 +2,16 @@ import { Menu, Item, useContextMenu } from 'react-contexify';
 import { successToast, errorToast } from '../toasts';
 import 'react-contexify/ReactContexify.css';
 import axios, * as others from 'axios'
+import 'reactjs-popup/dist/index.css'
+import Popup from 'reactjs-popup';
+import { useState } from 'react';
+
+import EditAuthor from '../edit/edit-author'
 
 function Authors({ authors, setQueryAuthorId, queryAuthorId, guildId, setAuthors }) {
+    const contentStyle = { background: '#2f3136', border: "#232428 2px solid", 'border-radius': '5px' }
+    const [authorBeingEdited, setAuthorBeingEdited] = useState(null)
+    const [showPop, setShowPopup] = useState(false)
     const authorContextId = 'author_id'
 
     function selectAuthor(id) {
@@ -27,6 +35,8 @@ function Authors({ authors, setQueryAuthorId, queryAuthorId, guildId, setAuthors
             break;
         case "edit":
             alert('Editing coming soon.')
+            setShowPopup(true)
+            setAuthorBeingEdited(props)
             break;
         case "delete":
             if (!window.confirm(`Delete "${name}"?`)) {
@@ -44,38 +54,54 @@ function Authors({ authors, setQueryAuthorId, queryAuthorId, guildId, setAuthors
         }
     }
 
-    return <div class='authors'>
-        <div class='authors_header'>Authors - { authors.length }</div>
-        <hr class="authors_divider"/>
-        <br/>
-        { authors.map(({ _id, iconURL, name }) => <div class='author'>
-            <div class={ queryAuthorId == _id ? 'highlighted' : 'unhighlighted'}>
-                <div class='author_container'
-                    onClick={() => selectAuthor(_id)}
-                    onContextMenu={(e) => handleContextMenu(e, { name, authorId: _id })}
-                >
-                    <img
-                        class='author_icon'
-                        src={iconURL || "/icon.png"}
-                        alt="author icon"
-                        width = "45"
-                        height = "45"
-                        onError={({ currentTarget }) => {
-                            currentTarget.onerror = null; // prevents looping
-                            currentTarget.src="/icon.png";
-                        }}
-                    />
-                    <div class='author_name'>{name}</div>
-                </div>
-                <Menu id={authorContextId} theme="dark">
-                    <Item id="copy" onClick={handleItemClick}>Copy</Item>
-                    <Item id="edit" onClick={handleItemClick}>Edit</Item>
-                    <Item id="delete" onClick={handleItemClick}>Delete</Item>
-                </Menu>
-            </div>
+    return <>
+        <div class='authors'>
+            <div class='authors_header'>Authors - { authors.length }</div>
+            <hr class="authors_divider"/>
             <br/>
-        </div>) }
-    </div>
+            { authors.map(({ _id, iconURL, name }) => <div class='author'>
+                <div class={ queryAuthorId == _id ? 'highlighted' : 'unhighlighted'}>
+                    <div class='author_container'
+                        onClick={() => selectAuthor(_id)}
+                        onContextMenu={(e) => handleContextMenu(e, { name, authorId: _id })}
+                    >
+                        <img
+                            class='author_icon'
+                            src={iconURL || "/icon.png"}
+                            alt="author icon"
+                            width = "45"
+                            height = "45"
+                            onError={({ currentTarget }) => {
+                                currentTarget.onerror = null; // prevents looping
+                                currentTarget.src="/icon.png";
+                            }}
+                        />
+                        <div class='author_name'>{name}</div>
+                    </div>
+                    <Menu id={authorContextId} theme="dark">
+                        <Item id="copy" onClick={handleItemClick}>Copy</Item>
+                        <Item id="edit" onClick={handleItemClick}>Edit</Item>
+                        <Item id="delete" onClick={handleItemClick}>Delete</Item>
+                    </Menu>
+                </div>
+                <br/>
+            </div>) }
+        </div>
+        <Popup
+            open={showPop}
+            position="center center"
+            modal onClose={() => setShowPopup(false)}
+            {...{ contentStyle }}
+        >
+            <EditAuthor
+                authorBeingEdited={authorBeingEdited}
+                guildId={guildId}
+                setAuthors={setAuthors}
+                authors={authors}
+                setAuthorBeingEdited={setAuthorBeingEdited}
+                />
+        </Popup>
+    </>
 }
 
 export default Authors;
