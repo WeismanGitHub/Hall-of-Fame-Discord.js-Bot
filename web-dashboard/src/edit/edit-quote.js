9a0a2a6526ce4408f42b866bee641dfcfab5ecf3
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-02-17 07:26:48 -0800

    created edit-quote.js

---
+++ web-dashboard/src/edit/edit-quote.js
@@ -0,0 +1,14 @@
+import { successToast, errorToast } from '../toasts';
+import axios, * as others from 'axios'
+import { useState } from 'react';
+
+function EditQuote({quoteBeingEdited, guildId, setQuotes, quotes, setQuoteBeingEdited}) {
+    function edit() {
+    }
+
+    return (<div class='edit_quote'>
+        edit quote
+    </div>)
+}
+
+export default EditQuote;
