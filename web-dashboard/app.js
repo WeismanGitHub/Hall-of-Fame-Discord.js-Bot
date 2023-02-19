70281b45791391648231b6ce69ab5c0878fcdc64
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-02-19 05:38:11 -0800

    allowed blob: in img-src csp

--- web-dashboard/app.js
+++ web-dashboard/app.js
@@ -24,7 +24,7 @@ app.use(helmet({
 	contentSecurityPolicy: {
 		directives: {
 			...helmet.contentSecurityPolicy.getDefaultDirectives(),
-			"img-src": ["*"],
+			"img-src": ["*", "blob:"],
 			'media-src': ['*']
 		},
 	},
