cbcdb429293b81d06571d076f3093168b1fbd51e
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-02-22 08:50:44 -0800

    builds now

--- web-dashboard/src/edit/edit-quote.js
+++ web-dashboard/src/edit/edit-quote.js
@@ -4,7 +4,7 @@ import { useState } from 'react';
 
 function EditQuote({quoteBeingEdited, guildId, setQuotes, quotes, setQuoteBeingEdited, authors, tags}) {
     const [authorId, setAuthorId] = useState(quoteBeingEdited.authorId)
-    const [tags, setTags] = useState(quoteBeingEdited.tags)
+    const [quoteTags, setQuoteTags] = useState(quoteBeingEdited.tags)
     const [text, setText] = useState(quoteBeingEdited.text)
     const [removeImage, setRemoveImage] = useState(false)
     const [selectedFile, setSelectedFile] = useState()
@@ -22,7 +22,7 @@ function EditQuote({quoteBeingEdited, guildId, setQuotes, quotes, setQuoteBeingE
     function edit() {
         const update = { removeImage }
 
-        if (text !== authorBeingEdited.text) {
+        if (text !== quoteBeingEdited.text) {
             update.newText = text
         }
         
@@ -81,73 +81,73 @@ function EditQuote({quoteBeingEdited, guildId, setQuotes, quotes, setQuoteBeingE
 
             setQuoteBeingEdited(updatedQuote)
         }).catch(err => {
-           errorToast(`Failed to edit "${authorBeingEdited.name}".`)
+            errorToast(`Failed to edit quote.`)
        })
     }
 
-    return (<>
-        <div class='author_edit_container'>
-            <div class='author_edit_preview'>
-                <img
-                    class='author_icon'
-                    src={(selectedFile ? URL.createObjectURL(selectedFile) : null) || authorBeingEdited.iconURL || "/icon.png"}
-                    alt="author icon"
-                    style={{'margin-right': '10px'}}
-                    width = "60"
-                    height = "60"
-                    onError={({ currentTarget }) => {
-                        currentTarget.onerror = null; // prevents looping
-                        currentTarget.src="/icon.png";
-                    }}
-                />
-                <div class='author_name'>{name}</div>
-            </div>
-            <br/>
-        </div>
+    // return (<>
+    //     <div class='author_edit_container'>
+    //         <div class='author_edit_preview'>
+    //             <img
+    //                 class='author_icon'
+    //                 src={(selectedFile ? URL.createObjectURL(selectedFile) : null) || authorBeingEdited.iconURL || "/icon.png"}
+    //                 alt="author icon"
+    //                 style={{'margin-right': '10px'}}
+    //                 width = "60"
+    //                 height = "60"
+    //                 onError={({ currentTarget }) => {
+    //                     currentTarget.onerror = null; // prevents looping
+    //                     currentTarget.src="/icon.png";
+    //                 }}
+    //             />
+    //             <div class='author_name'>{name}</div>
+    //         </div>
+    //         <br/>
+    //     </div>
 
-        <div class='centered_row'>
-            <label class="file_upload">
-                <input
-                    type="file"
-                    accept=".gif,.jpg,.jpeg,.png"
-                    onChange={(e) => setSelectedFile(e.target.files[0])}
-                    onKeyPress={ (event) => { event.key === 'Enter' && edit() } }
-                    hidden
-                />
-                Upload Icon
-            </label>
+    //     <div class='centered_row'>
+    //         <label class="file_upload">
+    //             <input
+    //                 type="file"
+    //                 accept=".gif,.jpg,.jpeg,.png"
+    //                 onChange={(e) => setSelectedFile(e.target.files[0])}
+    //                 onKeyPress={ (event) => { event.key === 'Enter' && edit() } }
+    //                 hidden
+    //             />
+    //             Upload Icon
+    //         </label>
             
-            <button
-                class={`popup_button ${deleteIcon ? 'highlighted' : 'unhighlighted'}`}
-                onClick={() => setDeleteIcon(!deleteIcon)}
-                style={{ 'min-width': '105px'}}
-                >
-                {`${deleteIcon == true ? 'Remove' : 'Keep'} Icon`}
-            </button>
+    //         <button
+    //             class={`popup_button ${deleteIcon ? 'highlighted' : 'unhighlighted'}`}
+    //             onClick={() => setDeleteIcon(!deleteIcon)}
+    //             style={{ 'min-width': '105px'}}
+    //             >
+    //             {`${deleteIcon == true ? 'Remove' : 'Keep'} Icon`}
+    //         </button>
 
-            <button
-                class={`popup_button ${removeAccountImage ? 'highlighted' : 'unhighlighted'}`}
-                onClick={() => setRemoveAccountImage(!removeAccountImage)}
-                style={{ 'min-width': '180px'}}
-                >
-                {`${removeAccountImage == true ? 'Remove' : 'Keep'} Account Image`}
-            </button>
+    //         <button
+    //             class={`popup_button ${removeAccountImage ? 'highlighted' : 'unhighlighted'}`}
+    //             onClick={() => setRemoveAccountImage(!removeAccountImage)}
+    //             style={{ 'min-width': '180px'}}
+    //             >
+    //             {`${removeAccountImage == true ? 'Remove' : 'Keep'} Account Image`}
+    //         </button>
         
-            <div style={{ width: '100%' }}><br/></div>
+    //         <div style={{ width: '100%' }}><br/></div>
 
-            <input
-                type="text"
-                class='edit_author_name'
-                value={name}
-                onChange={ (e)=> setName(e.target.value) }
-                onKeyPress={ (event) => { event.key === 'Enter' && edit() } }
-                autoFocus
-                />
+    //         <input
+    //             type="text"
+    //             class='edit_author_name'
+    //             value={name}
+    //             onChange={ (e)=> setName(e.target.value) }
+    //             onKeyPress={ (event) => { event.key === 'Enter' && edit() } }
+    //             autoFocus
+    //             />
             
-            <div style={{ width: '100%' }}><br/></div>
-            <button class='modal_submit' onClick={edit} style={{ 'margin-bottom': '50px' }}>Submit</button>
-        </div>
-    </>)
+    //         <div style={{ width: '100%' }}><br/></div>
+    //         <button class='modal_submit' onClick={edit} style={{ 'margin-bottom': '50px' }}>Submit</button>
+    //     </div>
+    // </>)
 }
 
 export default EditQuote;
