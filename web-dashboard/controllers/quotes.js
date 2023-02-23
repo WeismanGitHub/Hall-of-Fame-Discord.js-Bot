637f678c5f0f1ec2ae05ec6003441edb9cfa9f8b
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-02-23 08:10:01 -0800

    created createQuote func

--- web-dashboard/controllers/quotes.js
+++ web-dashboard/controllers/quotes.js
@@ -68,4 +68,22 @@ async function editQuote(req, res) {
 	res.status(200).end()
 }
 
-module.exports = { getQuotes, deleteQuote, editQuote }
+async function createQuote(req, res) {
+	const { type } = req.body.type
+
+	switch (type) {
+		case 'regular':
+			console.log('regular')
+			break;
+		case 'audio':
+			console.log('audio')
+			break;
+		case 'multi':
+			console.log('multi')
+			break;
+	}
+
+	res.status(200).end()
+}
+
+module.exports = { getQuotes, deleteQuote, editQuote, createQuote }
