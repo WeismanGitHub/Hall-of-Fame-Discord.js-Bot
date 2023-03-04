702c51367b5135996b5370fef270de51a092b406
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-03-04 04:09:15 -0800

    trim and lowercase tags

--- schemas/tag-schema.js
+++ schemas/tag-schema.js
@@ -2,4 +2,6 @@ module.exports = {
     type: String,
     minLength: 1,
     maxLength: 339, // A third of 1024 minus 6 for the ', ' used to join tags in the embed. Embed fields are max 1024 chars.
+    lowercase: true,
+    trim: true
 }
