ef2f971c5737c597544dfd5ebeedfc4904258b8e
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-03-07 23:19:45 -0800

    fixed missing space

--- schemas/filter-schema.js
+++ schemas/filter-schema.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const FilterSchema = new mongoose.Schema({
     query: { type: Object },
-    sort: { type: Object}
+    sort: { type: Object }
 }, { timestamps: { createdAt: true, updatedAt: false } });
 
 module.exports = mongoose.model('filters', FilterSchema);
