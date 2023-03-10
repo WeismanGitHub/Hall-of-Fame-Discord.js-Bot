7a71da98236433cee3604d57cc46b7dab301e619
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-03-10 05:17:56 -0800

    you cant not choose an author now

--- web-dashboard/src/popups/authors-popup.js
+++ web-dashboard/src/popups/authors-popup.js
@@ -1,12 +1,8 @@
 function AuthorsPopup({ authors, setQuoteAuthorId, quoteAuthorId }) {
-    function selectAuthor(id) {
-        quoteAuthorId == id ? setQuoteAuthorId(null) : setQuoteAuthorId(id)
-    }
-
     return (<>
         { authors.map(({ _id, iconURL, name }) => <div class='author'>
             <div class={ quoteAuthorId == _id ? 'highlighted' : 'unhighlighted'}>
-                <div class='author_container' onClick={() => selectAuthor(_id)}>
+                <div class='author_container' onClick={() => setQuoteAuthorId(_id)}>
                     <img
                         class='author_icon'
                         src={iconURL || "/icon.png"}
