4f42a6ba9a8b56dc3f93d6d4c0df99fc9c67d9eb
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-03-10 23:20:46 -0800

    nearly done

--- web-dashboard/src/edit/edit-quote.js
+++ web-dashboard/src/edit/edit-quote.js
@@ -7,6 +7,7 @@ import Popup from 'reactjs-popup';
 import FragmentsPopup from '../popups/fragments-popup'
 import AuthorsPopup from '../popups/authors-popup'
 import TagsPopup from '../popups/tags-popup'
+import DisplayQuote from '../display-quote'
 
 const contentStyle = {
     background:
@@ -18,14 +19,17 @@ const contentStyle = {
 }
 
 function EditQuote({quoteBeingEdited, guildId, setQuotes, quotes, setQuoteBeingEdited, authors, tags}) {
+    const [setQuoteFragments, quoteFragments] = useState(quoteBeingEdited.fragments)
     const [quoteAuthorId, setQuoteAuthorId] = useState(quoteBeingEdited.authorId)
     const [quoteTags, setQuoteTags] = useState(quoteBeingEdited.tags)
-    const [setQuoteFragments, quoteFragments] = useState(quoteBeingEdited.fragments)
     const [text, setText] = useState(quoteBeingEdited.text)
-    const [type, setType] = useState(quoteBeingEdited.type)
     const [removeImage, setRemoveImage] = useState(false)
-    const [selectedFile, setSelectedFile] = useState()
+    const attachmentURL = quoteBeingEdited.attachmentURL
+    const [imageFile, setImageFile] = useState(null)
+    const [audioFile, setAudioFile] = useState(null)
+    const createdAt = quoteBeingEdited.createdAt
     const quoteId = quoteBeingEdited._id
+    const type = quoteBeingEdited.type
 
     const [showFragmentsPopup, setShowFragmentsPopup] = useState(false)
     const [showAuthorsPopup, setShowAuthorsPopup] = useState(false)
@@ -47,8 +51,8 @@ function EditQuote({quoteBeingEdited, guildId, setQuotes, quotes, setQuoteBeingE
             update.newText = text
         }
         
-        if (selectedFile) {
-            getBase64(selectedFile)
+        if (imageFile) {
+            getBase64(imageFile)
             .then(base64File => {
                 axios.post(
                     'https://api.imgur.com/3/image/',
@@ -106,147 +110,25 @@ function EditQuote({quoteBeingEdited, guildId, setQuotes, quotes, setQuoteBeingE
        })
     }
 
-    function DisplayQuote() {
-        // const author = authors.find(author => author._id == quote.authorId)
-        // const { type, attachmentURL, createdAt, text, audioURL, fragments, _id } = quote
-        // const filteredTags = quote.tags.filter(x => x !== null)
-        // const tags = quoteTags.length ? quoteTags.join(', ') : ['no tags']
-        // let color;
-
-        // switch(type) {
-        //     case 'regular':
-        //         color = '#8F00FF'
-        //         break
-        //     case 'audio':
-        //         color = '#00A64A'
-        //         break
-        //     case 'multi':
-        //         color = '#ff2e95'
-        //         break
-        // }
-        
-        // color = attachmentURL ? '#FF7B00' : color
-        
-        // if (type !== 'multi') {
-        //     return <>
-        //         <div className='quote_message'
-        //         style={{ 'border-left': `4px solid ${color}` }}
-        //         onContextMenu={(e) => handleContextMenu(e, quote)}>
-        //             <div className="quote_author_avatar">
-        //                 <img
-        //                     src={ author?.iconURL || "/icon.png" }
-        //                     alt="author icon"
-        //                     width = "50"
-        //                     height = "50"
-        //                     onError={({ currentTarget }) => {
-        //                         currentTarget.onerror = null; // prevents looping
-        //                         currentTarget.src="/icon.png";
-        //                     }}
-        //                 />
-        //             </div>
-        //             <div className="quote_message_content">
-        //                 <div>
-        //                     <span className="quote_author_info">
-        //                         <span className="quote_author_username">{ author?.name || 'Deleted Author' }</span>
-        //                     </span>
-        //                     <span className="quote_message_timestamp">
-        //                         { moment(createdAt).calendar() } - { tags }
-        //                     </span>
-        //                 </div>
-
-        //                 <div className="quote_message_body">
-        //                     { text }
-                            
-        //                     { !audioURL ? null : <AudioPlayer
-        //                         src={audioURL}
-        //                         showJumpControls={false}
-        //                         showSkipControls={false}
-        //                         autoPlayAfterSrcChange={false}
-        //                         hasDefaultKeyBindings={false}
-        //                         showFilledProgress={true}
-        //                         layout={'horizontal'}
-        //                         customVolumeControls={[]}
-        //                     /> }
-
-        //                     { !attachmentURL ? null : <img
-        //                         class='quote_image'
-        //                         src={ attachmentURL }
-        //                         alt="invalid image"
-        //                     />}
-        //                 </div>
-        //             </div>
-        //         </div>
-        //         <Menu id={quoteContextId} theme="dark">
-        //             <Item id="edit" onClick={handleItemClick}>Edit</Item>
-        //             <Item id="delete" onClick={handleItemClick}>Delete</Item>
-        //         </Menu>
-        //     </>
-        // } else {
-        //     return <>
-        //         <div className='quote_message'
-        //         style={{ 'border-left': `4px solid ${color}` }}
-        //         onContextMenu={(e) => handleContextMenu(e, { id: _id })}>
-        //             <div className="quote_message_content">
-        //                 <div>
-        //                     <span className="quote_author_info" style={{ 'margin-left': '10px' }}>
-        //                         <span className="quote_author_username">{ text }</span>
-        //                     </span>
-        //                     <span className="quote_message_timestamp">
-        //                         { moment(createdAt).calendar() } - { tags }
-        //                     </span>
-        //                 </div>
-        //                 <div className="quote_message_body">
-        //                     { fragments.map(({ authorId, text })=> {
-        //                         const author = authors.find(author => author._id == authorId)
-
-        //                         return <div className='quote_message'>
-        //                             <div className="quote_author_avatar">
-        //                                 <img
-        //                                     src={ author?.iconURL || "/icon.png" }
-        //                                     alt="author icon"
-        //                                     width = "40"
-        //                                     height = "40"
-        //                                     onError={({ currentTarget }) => {
-        //                                         currentTarget.onerror = null; // prevents looping
-        //                                         currentTarget.src="/icon.png";
-        //                                     }}
-        //                                 />
-        //                             </div>
-        //                             <div className="quote_message_content">
-        //                                 <div>
-        //                                     <span className="quote_author_info">
-        //                                         <span className="quote_author_username">{ author?.name || 'Deleted Author' }</span>
-        //                                     </span>
-        //                                 </div>
-
-        //                                 <div className="quote_message_body">
-        //                                     { text }
-        //                                 </div>
-        //                             </div>
-        //                         </div>
-        //                     })}
-
-        //                     { !attachmentURL ? null : <img
-        //                         class='quote_image'
-        //                         src={ attachmentURL }
-        //                         alt="invalid image"
-        //                     />}
-        //                 </div>
-        //             </div>
-        //         </div>
-        //     </>
-        // }
-    }
-
     return (<>
-        <DisplayQuote/>
+        <DisplayQuote
+            author={authors.find(author => author._id == quoteAuthorId)}
+            type={type}
+            tags={quoteTags}
+            attachmentURL={removeImage ? null : (imageFile || attachmentURL)}
+            text={text}
+            fragments={quoteFragments}
+            audioURL={audioFile}
+            authors={authors}
+            createdAt={createdAt}
+        />
 
         <div class='centered_row'>
             <label class="file_upload">
                 <input
                     type="file"
                     accept=".gif,.jpg,.jpeg,.png"
-                    onChange={(e) => setSelectedFile(e.target.files[0])}
+                    onChange={(e) => setImageFile(e.target.files[0])}
                     onKeyPress={ (event) => { event.key === 'Enter' && create() } }
                     hidden
                 />
