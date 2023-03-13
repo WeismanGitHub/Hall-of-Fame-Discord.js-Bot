d38e055d1b2efef413df2ad319e89ea0823a83d4
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-03-13 13:55:29 -0700

    changed fragments popup dimensions

--- web-dashboard/src/edit/edit-quote.js
+++ web-dashboard/src/edit/edit-quote.js
@@ -251,7 +251,7 @@ function EditQuote({quoteBeingEdited, guildId, setQuotes, quotes, setQuoteBeingE
             open={showFragmentsPopup}
             position="center center"
             modal onClose={() => setShowFragmentsPopup(false)}
-            {...{ contentStyle: {...contentStyle, width: '400px' } }}
+            {...{ contentStyle: {...contentStyle, width: '700px', height: '500px' } }}
             nested
         >
             <FragmentsPopup
