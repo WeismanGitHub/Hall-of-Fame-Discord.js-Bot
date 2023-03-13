3e3dc11077dad6ccbd52410b77e306a624f7dba4
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-03-13 13:55:39 -0700

    finished display

--- web-dashboard/src/popups/fragments-popup.js
+++ web-dashboard/src/popups/fragments-popup.js
@@ -1,5 +1,15 @@
 function FragmentsPopup({ authors, setQuoteFragments, quoteFragments }) {
-    console.log(quoteFragments)
+    function deleteFragment() {
+        console.log('delete fragment')
+    }
+
+    function createFragment() {
+        console.log('new fragment')
+    }
+
+    function changeAuthor() {
+        console.log('change author')
+    }
 
     return (<>
         <div className="quote_message_body">
@@ -7,6 +17,23 @@ function FragmentsPopup({ authors, setQuoteFragments, quoteFragments }) {
                 const author = authors.find(author => author._id == authorId)
 
                 return <div className='quote_message' style={{'background-color': '#292c30'}}>
+                    <div style={{width: '100px', padding: '5px'}}>
+                        <button
+                            style={{padding: '5px'}}
+                            class='modal_submit'
+                            onClick={()=> changeAuthor({ authorId, text })}
+                        >
+                            Author
+                        </button>
+                        <button
+                            class='modal_submit'
+                            onClick={()=> deleteFragment({ authorId, text })}
+                            style={{padding: '5px', 'margin-top': '3px'}}
+                        >
+                            Delete
+                        </button>
+                    </div>
+
                     <div className="quote_author_avatar">
                         <img
                             src={ author?.iconURL || "/icon.png" }
@@ -26,11 +53,10 @@ function FragmentsPopup({ authors, setQuoteFragments, quoteFragments }) {
                             </span>
                         </div>
 
-                        <div className="quote_message_body">
+                        <div className="quote_message_body" style={{padding: '3px'}}>
                             <input
                                 type="text"
                                 class='edit_author_name'
-                                // style={{ width: '550px' }}
                                 value={text}
                                 onChange={ (e)=> console.log(e.target.value) }
                                 autoFocus
@@ -39,6 +65,10 @@ function FragmentsPopup({ authors, setQuoteFragments, quoteFragments }) {
                     </div>
                 </div>
             })}
+        </div>
+
+        <div class='centered_row'>
+            <button class='modal_submit' onClick={createFragment} style={{ 'margin-bottom': '50px' }}>New Fragment</button>
         </div>
     </>)
 }
