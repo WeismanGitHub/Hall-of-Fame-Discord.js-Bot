aa1da191c37819b946258acfc8c4c2e299de70f7
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-03-20 13:14:17 -0700

    completed loadCommands func

--- helpers/load.js
+++ helpers/load.js
@@ -1,8 +1,54 @@
-function loadCommands() {
+const { REST, Routes } = require('discord.js');
+const { readdirSync } = require('fs');
+const { join } = require('path');
 
+async function loadCommands(client) {
+    const commandsPath = join(__dirname, '../commands')
+    const commandFiles = readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+    const commands = [];
+
+    for (const file of commandFiles) {
+        const command = require(`../commands/${file}`);
+
+        if (!command?.data) {
+            continue
+        }
+
+        commands.push(command.data.toJSON());
+        client.commands.set(command.data.name, command);
+    }
+
+    console.log(`Started refreshing ${commands.length} application (/) commands.`);
+
+    const rest = new REST({ version: '10' }).setToken(client.token);
+    const clientId = client?.application?.id
+
+
+    if (!clientId) {
+        throw new Error('Client Id is invalid.')
+    }
+
+    await rest.put(Routes.applicationCommands(clientId), { body: commands });
+
+    console.log(`Successfully reloaded ${commands.length} application (/) commands.`);
+
+    client.on('interactionCreate', async interaction => {
+        if (!interaction.isCommand()) return;
+    
+        const command = client.commands.get(interaction.commandName);
+    
+        if (!command) return;
+    
+        try {
+            await command.execute(interaction);
+        } catch (err) {
+            console.error(err);
+            await interaction.reply({ content: 'There was an error while executing client command!', ephemeral: true });
+        }
+    });
 }
 
-function loadEvents() {
+async function loadEvents(client) {
 
 }
 
