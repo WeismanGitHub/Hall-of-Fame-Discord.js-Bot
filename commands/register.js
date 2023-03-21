b9f5ca3072b208b2857762e629c490aaf7c749b9
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-03-20 17:40:12 -0700

    converted /register to v14

--- commands/register.js
+++ commands/register.js
@@ -1,18 +1,18 @@
 const errorHandler = require('../helpers/error-handler');
 const GuildSchema = require('../schemas/guild-schema');
+const { SlashCommandBuilder } = require('discord.js');
 const { basicEmbed } = require('../helpers/embeds');
 
 module.exports = {
-    category:'Register',
-    name: 'register',
-    description: 'Register your server.',
-    guildOnly: true,
-    slash: true,
-
-    callback: async ({ interaction }) => errorHandler(interaction, async () => {
+	data: new SlashCommandBuilder()
+		.setName('register')
+		.setDescription('Register your server.')
+        .setDMPermission(false)
+	,
+	execute: async (interaction) => errorHandler(interaction, async () => {
         const guildId = interaction.guildId;
 
-        if (!await GuildSchema.findOne({ _id: guildId }).select('_id').lean()) {
+        if (!await GuildSchema.exists({ _id: guildId })) {
             await GuildSchema.create({ _id: guildId })
 
             await interaction.reply(basicEmbed('Registered This Server!'));
