f2147fb3b9159015677ff8c8090bc5bcc20955f0
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-03-20 17:41:05 -0700

    converted to discord.js v14

--- events/on-button-clicks/quotes-channel.js
+++ events/on-button-clicks/quotes-channel.js
@@ -1,33 +1,27 @@
-const { errorEmbed, basicEmbed } = require('../../helpers/embeds')
+const errorHandler = require('../../helpers/error-handler');
 const QuoteSchema = require('../../schemas/quote-schema')
 const sendQuotes = require('../../helpers/send-quotes')
+const { basicEmbed } = require('../../helpers/embeds')
+const { Events } = require('discord.js');
 
-module.exports =  (client) => {
-    client.on('interactionCreate', async (interaction) => {
-        try {
-            if (!interaction.isButton()) {
-                return
-            }
-
-            const { channelId, type } = JSON.parse(interaction.customId)
-            
-            if (type !== 'quotes-channel') {
-                return
-            }
-    
-            const channel = await client.channels.fetch(channelId)
-            const quotes = await QuoteSchema.find({ guildId: channel.guildId }).sort({ createdAt: 1 }).lean()
-            
-            await interaction.reply(basicEmbed('Designated Quotes Channel'))
-            await sendQuotes(quotes, channel)
-        } catch(err) {
-            await interaction.reply(errorEmbed(err))
-            .catch(_ => interaction.channel.send(errorEmbed(err)))
+module.exports = {
+	name: Events.InteractionCreate,
+	once: false,
+	execute: async (interaction) => errorHandler(interaction, async () => {
+        if (!interaction.isButton()) {
+            return
         }
-    })
-}
 
-module.exports.config = {
-    dbName: "on-button-click",
-    displayName: "on-button-click",
+        const { channelId, type } = JSON.parse(interaction.customId)
+        
+        if (type !== 'quotes-channel') {
+            return
+        }
+
+        const channel = await client.channels.fetch(channelId)
+        const quotes = await QuoteSchema.find({ guildId: channel.guildId }).sort({ createdAt: 1 }).lean()
+        
+        await interaction.reply(basicEmbed('Designated Quotes Channel'))
+        await sendQuotes(quotes, channel)
+	})
 }
