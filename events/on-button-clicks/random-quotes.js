154ff3a7773d8e7d7a0590c16d3b4fc83ec61b31
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-03-20 17:41:10 -0700

    converted to discord.js v14

--- events/on-button-clicks/random-quotes.js
+++ events/on-button-clicks/random-quotes.js
@@ -1,66 +1,60 @@
 const UniversalQuoteSchema = require('../../schemas/universal-quote-schema');
 const { MessageActionRow, MessageButton } = require('discord.js');
-const { errorEmbed, basicEmbed } = require('../../helpers/embeds');
+const errorHandler = require('../../helpers/error-handler');
 const FilterSchema = require('../../schemas/filter-schema');
 const sendQuotes = require('../../helpers/send-quotes');
+const { basicEmbed } = require('../../helpers/embeds');
 const { InvalidActionError } = require('../../errors');
+const { Events } = require('discord.js');
 
-module.exports = async (client, instance) => {
-    client.on('interactionCreate', async (interaction) => {
-        try {
-            if (!interaction.isButton()) {
-                return
-            }
-
-            const { type, filterId } = JSON.parse(interaction.customId)
-
-            if (type !== 'random-quotes') {
-                return
-            }
-            
-            const filter = await FilterSchema.findById(filterId).lean()
-
-            if (!filter) {
-                throw new InvalidActionError('Retry Command')
-            }
-
-            const { query } = filter
-
-            const quotes = await UniversalQuoteSchema.aggregate([
-                { $match: query },
-                { $sample: { size: 10 } }
-            ])
-
-            const customId = JSON.stringify({ type: 'random-quotes', filterId })
-
-            const row = new MessageActionRow()
-            .addComponents(
-                new MessageButton()
-                .setCustomId(`${customId}`)
-                .setLabel('Next 10 Random Quotes ⏩')
-                .setStyle('PRIMARY')
-            )
-    
-            await interaction.reply(basicEmbed('Started!'));
-
-            // sendQuotes modifies quotes array so gotta use a copy.
-            await sendQuotes([...quotes], interaction.channel)
-    
-            if (quotes.length !== 10) {
-                return await interaction.channel.send(basicEmbed('End of the line!'))
-            }
-    
-            await interaction.channel.send({
-                components: [row]
-            })
-        } catch(err) {
-            await interaction.reply(errorEmbed(err))
-            .catch(_ => interaction.channel.send(errorEmbed(err)))
+module.exports = {
+	name: Events.InteractionCreate,
+	once: false,
+	execute: async (interaction) => errorHandler(interaction, async () => {
+        if (!interaction.isButton()) {
+            return
         }
-    })
-}
 
-module.exports.config = {
-    dbName: "on-button-click",
-    displayName: "on-button-click",
-}
+        const { type, filterId } = JSON.parse(interaction.customId)
+
+        if (type !== 'random-quotes') {
+            return
+        }
+        
+        const filter = await FilterSchema.findById(filterId).lean()
+
+        if (!filter) {
+            throw new InvalidActionError('Retry Command')
+        }
+
+        const { query } = filter
+
+        const quotes = await UniversalQuoteSchema.aggregate([
+            { $match: query },
+            { $sample: { size: 10 } }
+        ])
+
+        const customId = JSON.stringify({ type: 'random-quotes', filterId })
+
+        const row = new MessageActionRow()
+        .addComponents(
+            new MessageButton()
+            .setCustomId(`${customId}`)
+            .setLabel('Next 10 Random Quotes ⏩')
+            .setStyle('PRIMARY')
+        )
+
+        await interaction.reply(basicEmbed('Started!'));
+
+        // sendQuotes modifies quotes array so gotta use a copy.
+        await sendQuotes([...quotes], interaction.channel)
+
+        if (quotes.length !== 10) {
+            return await interaction.channel.send(basicEmbed('End of the line!'))
+        }
+
+        await interaction.channel.send({
+            components: [row]
+        })
+	})
+};
