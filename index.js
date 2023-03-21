66640cf50d8c3bf85eb8743b2057bf7164485b60
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-03-20 19:02:16 -0700

    use connectDB func

--- index.js
+++ index.js
@@ -1,40 +1,28 @@
-const { Client, Intents } = require('discord.js');
-const WOKCommands = require('wokcommands');
+const { Client, GatewayIntentBits, Collection } = require('discord.js');
+const { loadCommands, loadEvents } = require('./helpers/load');
+const connectDB = require('./mongodb-connect')
 const app = require('./web-dashboard/app')
-const mongoose = require('mongoose');
 const log = require('log-to-file');
-const path = require('path');
 require('dotenv').config();
 
 const client = new Client({
     intents: [
-        Intents.FLAGS.GUILD_MESSAGES,
-        Intents.FLAGS.GUILDS,
-        Intents.FLAGS.GUILD_VOICE_STATES
-    ]
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.GuildVoiceStates
+    ],
 });
 
+client.commands = new Collection()
 const port = process.env.PORT || 5000
-app.listen(port || 5000, () => console.log(`listening on port ${port}...`));
 app.set('client', client)
-
-mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-}).then(console.log('connected to database...'));
 
 client.on("ready", async () => {
     client.user.setActivity('Use /help for help.');
+    console.log('bot is ready...')
 
-	new WOKCommands(
-        client, {
-            commandsDir: path.join(__dirname, './commands'),
-            featuresDir: path.join(__dirname, './events'),
-            testServers: [process.env.TEST_GUILD_ID],
-            botOwners: [process.env.MAIN_ACCOUNT_ID],
-            disabledDefaultCommands: ['prefix', 'language'],
-            mongoUri: process.env.MONGO_URI,
-    });
+    loadCommands(client)
+    loadEvents(client)
 });
 
 process.on('uncaughtException', function (error) {
@@ -47,6 +35,8 @@ process.on('unhandledRejection', error => {
     console.log(new Date, error)
 });
 
+connectDB()
 client.login(process.env.TOKEN);
+app.listen(port || 5000, () => console.log(`listening on port ${port}...`));
 
 module.exports = client
