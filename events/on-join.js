06c98c95b2f332f999624ce6f9c8a59a6f4c7624
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-03-20 17:41:15 -0700

    converted to discord.js v14

--- events/on-join.js
+++ events/on-join.js
@@ -1,28 +1,24 @@
+const errorHandler = require('../helpers/error-handler');
 const GuildSchema = require('../schemas/guild-schema');
 const { helpEmbed } = require('../helpers/embeds')
+const { Events } = require('discord.js');
 
-module.exports = (client) => {
-    client.on('guildCreate', async (guild) => {
-        try {
-            const guildId = guild.id;
-            const guildRegistered = await GuildSchema.exists({ _id: guildId })
+module.exports = {
+	name: Events.GuildCreate,
+	once: false,
+    execute: async (interaction) => errorHandler(interaction, async () => {
+        console.log(interaction)
+        // const guildId = guild.id;
+        // const guildRegistered = await GuildSchema.exists({ _id: guildId })
+    
+        // if (!guildRegistered) {
+        //     await GuildSchema.create({_id: guildId });
+        // }
+
+        // const channel = guild.systemChannelID ?? guild.channels.cache.find(channel => 
+        //     channel.type == 'GUILD_TEXT' && channel.permissionsFor(guild.me).has('SEND_MESSAGES')
+        // )
         
-            if (!guildRegistered) {
-                await GuildSchema.create({_id: guildId });
-            }
-
-            const channel = guild.systemChannelID ?? guild.channels.cache.find(channel => 
-                channel.type == 'GUILD_TEXT' && channel.permissionsFor(guild.me).has('SEND_MESSAGES')
-            )
-            
-            await channel.send(helpEmbed())
-        } catch(err) {
-            console.log(err)
-        }
+        // await channel.send(helpEmbed())
     })
-}
-
-module.exports.config = {
-    dbName: "on-join",
-    displayName: "on-join",
-}
+};
