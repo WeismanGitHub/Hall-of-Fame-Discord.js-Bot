4430e02e25b7151d6cdc55ad7d89ffcbfb67b685
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-03-20 17:40:48 -0700

    converted to discord.js v14

--- events/on-button-clicks/find-quotes.js
+++ events/on-button-clicks/find-quotes.js
@@ -1,64 +1,58 @@
 const UniversalQuoteSchema = require('../../schemas/universal-quote-schema');
 const { MessageActionRow, MessageButton } = require('discord.js');
-const { errorEmbed, basicEmbed } = require('../../helpers/embeds');
 const FilterSchema = require('../../schemas/filter-schema');
+const errorHandler = require('../../helpers/error-handler');
 const sendQuotes = require('../../helpers/send-quotes');
 const { InvalidActionError } = require('../../errors');
+const { basicEmbed } = require('../../helpers/embeds');
+const { Events } = require('discord.js');
 
-module.exports = async (client, instance) => {
-    client.on('interactionCreate', async (interaction) => {
-        try {
-            if (!interaction.isButton()) {
-                return
-            }
-
-            const { type, skipAmount, filterId } = JSON.parse(interaction.customId)
-
-            if (type !== 'find-quotes') {
-                return
-            }
-            
-            const filter = await FilterSchema.findById(filterId).lean()
-
-            if (!filter) {
-                throw new InvalidActionError('Retry Command')
-            }
-
-            const { query, sort } = filter
-
-            const quotes = await UniversalQuoteSchema.find(query)
-            .sort(sort).skip(skipAmount).limit(10).lean();
-
-            const customId = JSON.stringify({ type: 'find-quotes', filterId: filterId, skipAmount: Number(skipAmount) + 10 })
-
-            const row = new MessageActionRow()
-            .addComponents(
-                new MessageButton()
-                .setCustomId(`${customId}`)
-                .setLabel('Next 10 Quotes ⏩')
-                .setStyle('PRIMARY')
-            )
-    
-            await interaction.reply(basicEmbed('Started!'));
-
-            // sendQuotes modifies quotes array so gotta use a copy.
-            await sendQuotes([...quotes], interaction.channel)
-    
-            if (quotes.length !== 10) {
-                return await interaction.channel.send(basicEmbed('End of the line!'))
-            }
-    
-            await interaction.channel.send({
-                components: [row]
-            })
-        } catch(err) {
-            await interaction.reply(errorEmbed(err))
-            .catch(_ => interaction.channel.send(errorEmbed(err)))
+module.exports = {
+	name: Events.InteractionCreate,
+	once: false,
+    execute: async (interaction) => errorHandler(interaction, async () => {
+        if (!interaction.isButton()) {
+            return
         }
-    })
-}
 
-module.exports.config = {
-    dbName: "on-button-click",
-    displayName: "on-button-click",
+        const { type, skipAmount, filterId } = JSON.parse(interaction.customId)
+
+        if (type !== 'find-quotes') {
+            return
+        }
+        
+        const filter = await FilterSchema.findById(filterId).lean()
+
+        if (!filter) {
+            throw new InvalidActionError('Retry Command')
+        }
+
+        const { query, sort } = filter
+
+        const quotes = await UniversalQuoteSchema.find(query)
+        .sort(sort).skip(skipAmount).limit(10).lean();
+
+        const customId = JSON.stringify({ type: 'find-quotes', filterId: filterId, skipAmount: Number(skipAmount) + 10 })
+
+        const row = new MessageActionRow()
+        .addComponents(
+            new MessageButton()
+            .setCustomId(`${customId}`)
+            .setLabel('Next 10 Quotes ⏩')
+            .setStyle('PRIMARY')
+        )
+
+        await interaction.reply(basicEmbed('Started!'));
+
+        // sendQuotes modifies quotes array so gotta use a copy.
+        await sendQuotes([...quotes], interaction.channel)
+
+        if (quotes.length !== 10) {
+            return await interaction.channel.send(basicEmbed('End of the line!'))
+        }
+
+        await interaction.channel.send({
+            components: [row]
+        })
+    })
 }
