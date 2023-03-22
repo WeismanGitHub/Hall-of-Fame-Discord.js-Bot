8d1985f84fd519f9fbf06109b353f03c5928396d
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-03-21 23:43:41 -0700

    updated /create_tag to v14

--- commands/tag-commands/create-tag.js
+++ commands/tag-commands/create-tag.js
@@ -1,27 +1,24 @@
 const GuildSchema = require('../../schemas/guild-schema');
+const { tagDescription } = require('../../descriptions');
 const { basicEmbed } = require('../../helpers/embeds');
+const { SlashCommandBuilder } = require('discord.js');
 
 module.exports = {
-    category:'Tags',
-    name: 'create_tag',
-    description: 'Creates a tag. Tags can be used to classify quotes.',
-    guildOnly: true,
-    slash: true,
-
-    options: [
-        {
-            name: 'tag',
-            description: 'The name of the tag you want to create.',
-            required: true,
-    //        type: Constants.ApplicationCommandOptionTypes.STRING,
-            maxLength: 339,
-        }
-    ],
-
-    callback: async (interaction) => {
+	data: new SlashCommandBuilder()
+		.setName('create_tag')
+		.setDescription('Creates a tag. Tags can be used to classify quotes.')
+        .setDMPermission(false)
+        .addStringOption(option => option
+            .setName('tag')
+            .setDescription(tagDescription)
+            .setMaxLength(339)
+            .setRequired(true)
+        )
+	,
+	execute: async (interaction) => {
         const { options } = interaction;
         const guildId = interaction.guildId;
-        const tag = (options.getString('tag')).toLowerCase();
+        const tag = (options.getString('tag'))?.toLowerCase();
 
         await GuildSchema.updateOne(
             { _id: guildId },
