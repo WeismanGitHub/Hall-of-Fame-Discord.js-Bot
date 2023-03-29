11b9785919ea67b1519863f7f7dba084295c3eda
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-03-29 01:12:05 -0700

    added extra safety check

--- helpers/send-to-quotes-channel.js
+++ helpers/send-to-quotes-channel.js
@@ -8,6 +8,10 @@ async function updateQuotesChannel(quote, guildId, client) {
         const quotesChannel = await client.channels.fetch(quotesChannelId)
         .catch(err => { throw new Error("Can't send quotes to quotes channel.") })
 
+        if (channel.guildId !== guildId) {
+			throw new Error('Quotes channel is not in this server.')
+		}
+
         await quotesChannel.send(quote)
     }
 }
